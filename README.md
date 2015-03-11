2c1925f24147fd6222a438e908d4950d26c0f555
David Irvine <david.irvine@maidsafe.net>
2015-03-11 14:32:09 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# maidsafe_vault
+
+Travis build status
+
+[![Build Status](https://travis-ci.org/dirvine/maidsafe_vault.svg?branch=master)](https://travis-ci.org/dirvine/maidsafe_vault)
+
+Appveyor build status (Windows)
+
+[![Build status](https://ci.appveyor.com/api/projects/status/jsuo65sa631h0kav?svg=true)](https://ci.appveyor.com/project/dirvine/maidsafe-vault)
+
+
+[Documentation](http://dirvine.github.io/maidsafe_vault/)
+
+
+#Todo
---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#![crate_name = "maidsafe_vault"]
+#![crate_type = "lib"]
+#![doc(html_logo_url = "http://maidsafe.net/img/Resources/branding/maidsafe_logo.fab2.png",
+       html_favicon_url = "http://maidsafe.net/img/favicon.ico",
+              html_root_url = "http://dirvine.github.io/dirvine/maidsafe_vault/")]
+//! Placeholder
+
+
+/// Placeholder doc test
+pub fn always_true() -> bool { true }
+
+#[test]
+fn it_works() {
+ assert_eq!(always_true(), true);
+}
