914b5bd4441de87ca60fd34305642d3dee1b88f9
maqi <qi.ma@maidsafe.net>
2015-03-17 17:23:48 +0000

    pass compilation with cargo build

--- src/lib.rs
+++ src/lib.rs
@@ -33,18 +33,16 @@
 #![doc(html_logo_url = "http://maidsafe.net/img/Resources/branding/maidsafe_logo.fab2.png",
        html_favicon_url = "http://maidsafe.net/img/favicon.ico",
               html_root_url = "http://dirvine.github.io/routing")]
-#![warn(missing_docs)]
-#![feature(io, collections, slicing_syntax, custom_derive)]
+// #![warn(missing_docs)]
+#![feature(custom_derive, net)]
+#![allow(dead_code, unused_variables)]
 
 extern crate sodiumoxide;
 extern crate "rustc-serialize" as rustc_serialize;
 extern crate cbor;
 extern crate utp;
 
-use std::str::FromStr;
-use std::io::{stdin, stdout, stderr, Write};
-use std::net::{TcpListener, TcpStream, IpAddr, SocketAddr};
-use std::thread;
+use std::net::{TcpStream};
 use sodiumoxide::crypto;
 use std::sync::mpsc;
 use std::sync::mpsc::{Sender, Receiver};
@@ -167,6 +165,6 @@ fn facade_implementation() {
     fn handle_post_response(&self, from_authority: Authority,from_address: DhtIdentity , response: Result<Vec<u8>, RoutingError>) { unimplemented!(); }  
   } 
   let my_facade = MyFacade;
-  let mut my_routing = RoutingNode::new(&my_facade as & Facade);
+  let my_routing = RoutingNode::new(&my_facade as & Facade);
   /* assert_eq!(999, my_routing.get_facade().handle_get_response());  */
 }
