21381c100fcfc9ac1ce68f077b0a3753d13b3aa8
maqi <qi.ma@maidsafe.net>
2015-03-25 21:21:05 +0000

    make beacon a simple broadcast listener

--- src/beacon.rs
+++ src/beacon.rs
@@ -18,71 +18,35 @@ extern crate cbor;
 use std::net::{UdpSocket, Ipv4Addr, SocketAddr, SocketAddrV4};
 use std::io::Result as IoResult;
 use std::io::Error as IoError;
-use std::io::{ Error };
-use cbor::{ CborError }; 
 use std::thread::spawn;
-use std::marker::PhantomData;
-use rustc_serialize::{ Decodable, Encodable };
+use rustc_serialize::{ Encodable };
 use bchannel::channel;
 
 pub use bchannel::Receiver;
-pub type InBeaconStream<T> = Receiver<T, CborError>;
-
-pub fn array_as_vector(arr: &[u8]) -> Vec<u8> {
-  let mut vector = Vec::new();
-  for i in arr.iter() {
-    vector.push(*i);
-  }
-  vector
-}
 
 pub struct Notification {
   peer_addr : SocketAddr,
-  buf : [u8; 1048576]
+  buf : [u8; 1024 * 1024]
 }
 
-pub struct OutBeaconStream<T> {
-  udp_socket: UdpSocket,
-  peer_addr : SocketAddr,
-  _phantom: PhantomData<T>
-}
-
-impl <'a, T> OutBeaconStream<T> where T: Encodable {
-  pub fn send(&mut self, m: &T) -> Result<usize, Error> {
-    let mut e = cbor::Encoder::from_memory();
-    e.encode(&[&m]).unwrap();
-    self.udp_socket.send_to(e.as_bytes(), self.peer_addr)
-  }
-
-  pub fn close(self) {
-    // instead of closing the whole socket, stop expecting msg from it
-    // drop(self.udp_socket);
-    if (self.udp_socket.leave_multicast(&self.peer_addr)).is_err() {
-      panic!("can not disconnect the specified udp peer");
-    }
-  }
-}
-
-/// Connect to a server and open a send-receive pair.  See `upgrade` for more details.
-pub fn connect_udp<'a, 'b, I, O>(addr: SocketAddr, peer_addr: SocketAddr) ->
-  IoResult<(Receiver<I, CborError>, OutBeaconStream<O>)>
-      where I: Send + Decodable + 'static, O: Encodable {
-  Ok(try!(upgrade_udp(try!(UdpSocket::bind(&addr)), peer_addr)))
-}
-
-pub fn listen()  -> IoResult<(Receiver<Notification, IoError>, UdpSocket)> {
-  let live_address = SocketAddrV4::new(Ipv4Addr::new(0,0,0,0), 5483);
-  // let any_address = SocketAddrV4::new(Ipv4Addr::new(0,0,0,0), 0);
+pub fn broadcast<T>(m: &T) -> IoResult<Receiver<Notification, IoError>> where T: Encodable {
+  let live_address = SocketAddrV4::new(Ipv4Addr::new(127,0,0,1), 5483);
+  let any_address = SocketAddrV4::new(Ipv4Addr::new(0,0,0,0), 0);
   let socket = try!(UdpSocket::bind(live_address));
+  let mut e = cbor::Encoder::from_memory();
+  e.encode(&[&m]).unwrap();
+  let send_result = socket.send_to(e.as_bytes(), any_address);
+  if send_result.is_err() {
+    return Err(send_result.err().unwrap());
+  }
   let (tx, rx) = channel();
-  let socket2 = try!(socket.try_clone());
   spawn(move || {
     loop {
       if tx.is_closed() {
         break;
       }
       let mut buf = [0; 1024 * 1024];
-      let result = socket2.recv_from(&mut buf);
+      let result = socket.recv_from(&mut buf);
       if result.is_ok() {
         let (amt, src) = result.unwrap();
         if amt > 0 {
@@ -90,58 +54,9 @@ pub fn listen()  -> IoResult<(Receiver<Notification, IoError>, UdpSocket)> {
             continue;
           }
         }
-        // put the new sender under monitor
-        if socket2.join_multicast(&src).is_err() {
-          panic!("can not expecting another udp peer");
-        }
       }
     }
-    drop(socket2);
+    drop(socket);
   });
-  Ok((rx, socket))
+  Ok(rx)
 }
-
-
-// Almost a straight copy of https://github.com/TyOverby/wire/blob/master/src/tcp.rs
-/// Upgrades an UdpSocket to a Sender-Receiver pair that you can use to send and
-/// receive objects automatically.  If there is an error decoding or encoding
-/// values, that respective part is shut down.
-pub fn upgrade_udp<'a, 'b, I, O>(udp_socket: UdpSocket, peer_addr: SocketAddr)
-    -> IoResult<(InBeaconStream<I>, OutBeaconStream<O>)> where I: Send + Decodable + 'static, O: Encodable {
-  let s1 = udp_socket;
-  let s2 = try!(s1.try_clone());
-  Ok((upgrade_reader(s1, peer_addr), upgrade_writer(s2, peer_addr)))
-}
-
-fn upgrade_writer<'a, T>(udp_socket: UdpSocket, peer_addr: SocketAddr) -> OutBeaconStream<T> where T: Encodable {
-  OutBeaconStream { udp_socket: udp_socket, peer_addr: peer_addr, _phantom: PhantomData }
-}
-
-fn upgrade_reader<'a, T>(socket: UdpSocket, peer_address : SocketAddr)
-    -> InBeaconStream<T> where T: Send + Decodable + 'static {
-  let (in_snd, in_rec) = channel();
-  spawn(move || {
-    let mut buf = [0; 1024 * 1024];
-    loop {
-      let result = socket.recv_from(&mut buf);
-      if result.is_ok() {
-        let (amt, src) = result.unwrap();
-        // only process the incoming msg when it is expected
-        if amt > 0 && src == peer_address {
-          let mut d = cbor::Decoder::from_bytes(array_as_vector(&buf));
-          let received: T = d.decode().next().unwrap().unwrap();
-          if in_snd.send(received).is_err() {
-            break;
-          }
-        }
-      }
-    }
-    // instead of closing the whole socket, stop expecting msg from it
-    // drop(self.udp_socket);
-    if (socket.leave_multicast(&peer_address)).is_err() {
-      panic!("can not disconnect the specified udp peer");
-    }
-  });
-  in_rec
-}
-
