5d2b9b3218432b0332e1c49a3e05f132038f2937
ustulation <ustulation@gmail.com>
2015-03-27 18:08:02 +0530

    vault depends on routing

--- src/vault.rs
+++ src/vault.rs
@@ -13,4 +13,56 @@
     use of the MaidSafe
     Software.                                                                 */
 
+extern crate Routing;
+extern crate sodiumoxide;
+extern crate "lru-cache" as lru_cache;
+extern crate "rustc-serialize" as rustc_serialize;
+extern crate cbor;
+extern crate time;
+extern crate bchannel;
+
+use std::net::{TcpStream};
+use sodiumoxide::crypto;
+use std::sync::mpsc;
+use std::sync::mpsc::{Sender, Receiver};
+use std::default::Default;
+
+
+use pmid_node::PmidNode;
+use self::Routing::Authority;
+use self::Routing::DhtIdentity;
+use self::Routing::Action;
+use self::Routing::RoutingError;
+
+impl Routing::Facade for PmidNode {
+  fn handle_get(&self, our_authority: Authority, from_authority: Authority, from_address: DhtIdentity, data: Vec<u8>)->Result<Action, RoutingError> {
+    let get_result = self.store.get(data);
+    Ok(Action::Reply(get_result))
+  }
+
+  fn handle_put(&mut self, our_authority: Authority, from_authority: Authority, from_address: DhtIdentity, data: Vec<u8>)->Result<Action, RoutingError> {
+	let mut e = cbor::Encoder::from_memory();
+	e.encode(&data).unwrap();
+    self.store.put(data, e.into_bytes());
+    Ok(Action::SendOn(from_address))
+  }
+
+  fn handle_post(&self, our_authority: Authority, from_authority: Authority, from_address: DhtIdentity, data: Vec<u8>)->Result<Action, RoutingError> {
+    ;
+    Err(RoutingError::InvalidRequest)
+  }
+
+  fn handle_get_response(&self, from_address: DhtIdentity, response: Result<Vec<u8>, RoutingError>) {
+    ;
+  }
+
+  fn handle_put_response(&self, from_authority: Authority, from_address: DhtIdentity, response: Result<Vec<u8>, RoutingError>) {
+    ;
+  }
+
+  fn handle_post_response(&self, from_authority: Authority, from_address: DhtIdentity, response: Result<Vec<u8>, RoutingError>) {
+    ;
+  }
+}
+
 
