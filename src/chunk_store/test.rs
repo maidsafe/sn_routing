ea93c98f8eec989f994bb52a5a3a9dac051a1bad
KrishnaIndia <krishnaof1988@gmail.com>
2015-04-02 11:01:42 +0530

    suggested changes implemented

--- src/chunk_store/test.rs
+++ src/chunk_store/test.rs
@@ -7,10 +7,10 @@ use chunk_store::ChunkStore;
 use std::path::Path;
 use std::fs::{remove_dir_all};
 
-static ONE_KB: u64 = 1024u64;
-static AES_PADDING: u64 = 16u64;
+static ONE_KB: u64 = 1024;
+
 // Allow 16 bytes extra per chunk since we're AES encrypting them
-static K_DEFAULT_MAX_DISK_USAGE: u64 = 4 * 1024u64 * 16u64; // 4 * (OneKB + AesPadding);
+static K_DEFAULT_MAX_DISK_USAGE: u64 = 4 * 1024u64;// * 16u64; // 4 * (OneKB + AesPadding);
 
 struct NameValueContainer(Vec<(maidsafe_types::NameType, String)>);
 
@@ -35,13 +35,12 @@ fn add_random_name_value_pairs(number: usize, size: usize) -> NameValueContainer
         container.push((get_random_name_type(), get_random_non_empty_string(size)));
         i = i + 1; // i++; is not compiling
         if i == number {
-          break;
+            break;
         }
     }
     NameValueContainer(container)
 }
 
-// TODO (Krishna) - Must support file storage, once the ChunkStore supports file storage. At present ChunkStore is in-memory storage
 struct ChunkStoreTest {
     chunk_store: ChunkStore,
     max_disk_storage: u64
@@ -50,8 +49,8 @@ struct ChunkStoreTest {
 impl ChunkStoreTest {
     pub fn new() -> ChunkStoreTest {
         ChunkStoreTest {
-        chunk_store: ChunkStore::new(),
-        max_disk_storage: K_DEFAULT_MAX_DISK_USAGE
+            chunk_store: ChunkStore::new(),
+            max_disk_storage: K_DEFAULT_MAX_DISK_USAGE
         }
     }
 
@@ -59,16 +58,12 @@ impl ChunkStoreTest {
         match remove_dir_all(dir_path) {
             Ok(_) => true,
             Err(_) => false
-          }
+        }
     }
 
     fn populate_chunk_store(&mut self, num_entries: u32, disk_entries: u32) -> NameValueContainer {
         let mut name_value_pairs = add_random_name_value_pairs(num_entries as usize, ONE_KB as usize);
-        // DiskUsage disk_usage(disk_entries * (OneKB + AesPadding)); C++ equivalent of the line below.
-        // Must check what DiskUsage type actually is
         let disk_usage = (disk_entries as u64) * (ONE_KB + AES_PADDING);
-        // chunk_store_.reset(new ChunkStore(chunk_store_path_, disk_usage));
-        // CLARIFY :: Next two lines are the conversion of the above C++ equivalent.
         self.chunk_store = ChunkStore::new();
         self.chunk_store.set_max_disk_usage(K_DEFAULT_MAX_DISK_USAGE as usize);
         for name_value in name_value_pairs.0.clone() {
