6e8c0bde6d97681dc3b1b58d5a9ab5632371453a
ustulation <ustulation@gmail.com>
2015-04-03 15:36:48 +0530

    commenting out stuff that breaks build

--- examples/simple_server.rs
+++ examples/simple_server.rs
@@ -12,20 +12,20 @@ fn fib(n: u64) -> u64 {
 
 fn main() {
     // Make a listener on 0.0.0.0:8080
-    let (listener, _) = routing::tcp_connections::listen().unwrap();
+    //let (listener, _) = routing::tcp_connections::listen().unwrap();
 
 
-    // Turn the listener into an iterator of connections.
-    for (connection, _) in listener.into_blocking_iter() {
-        // Spawn a new thread for each connection that we get.
-        spawn(move || {
-            // Upgrade the connection to read `u64` and write `(u64, u64)`.
-            let (i, mut o) = routing::tcp_connections::upgrade_tcp(connection).unwrap();
-            // For each `u64` that we read from the network...
-            for x in i.into_blocking_iter() {
-                // Send that number back with the computed value.
-                o.send(&(x, fib(x))).ok();
-            }
-        });
-    }
+    //// Turn the listener into an iterator of connections.
+    //for (connection, _) in listener.into_blocking_iter() {
+    //    // Spawn a new thread for each connection that we get.
+    //    spawn(move || {
+    //        // Upgrade the connection to read `u64` and write `(u64, u64)`.
+    //        let (i, mut o) = routing::tcp_connections::upgrade_tcp(connection).unwrap();
+    //        // For each `u64` that we read from the network...
+    //        for x in i.into_blocking_iter() {
+    //            // Send that number back with the computed value.
+    //            o.send(&(x, fib(x))).ok();
+    //        }
+    //    });
+    //}
 }
