92ad0fa63027481940827bf3a29258842d6cc13b
Ross Muir <ross.muir@maidsafe.net>
2015-04-24 16:56:17 +0100

    Update installer scripts

--- installer/linux/scripts/rpm_x64.sh
+++ installer/linux/scripts/rpm_x64.sh
@@ -2,5 +2,6 @@
 #
 # Create an RPM package for Vault Release binaries
 #
-# TODO update -C to pick up correct files when vault completed
-fpm -s dir -t rpm -C ../../target/release --name safe_network_vault --version 0.0.3 --license GPLv3 --vendor MaidSafe --maintainer qa@maidsafe.net --prefix $HOME/MaidSafe --description "The Ants are coming!!" --url "http://maidsafe.net"
+# Requires FPM installed > https://github.com/jordansissel/fpm
+#
+fpm -s dir -t rpm -C ../../target/release/ --name safe_network_vault --version 0.0.3 --license GPLv3 --vendor MaidSafe --maintainer qa@maidsafe.net --prefix $HOME/MaidSafe --description "The Ants are coming!!" --url "http://maidsafe.net" maidsafe_vault
--- installer/osx/scripts/osxpkg_x64.sh
+++ installer/osx/scripts/osxpkg_x64.sh
@@ -2,5 +2,6 @@
 #
 # Create an OSX package for Vault Release binaries
 #
-# TODO update -C to pick up correct files when vault completed
-fpm -s dir -t osxpkg -C ../../target/release --name safe_network_vault --version 0.0.3 --license GPLv3 --vendor MaidSafe --maintainer qa@maidsafe.net --prefix $HOME/MaidSafe --description "The Ants are coming!!" --url "http://maidsafe.net"
+# Requires FPM installed > https://github.com/jordansissel/fpm
+#
+fpm -s dir -t osxpkg -C ../../target/release/ --name safe_network_vault --version 0.0.3 --license GPLv3 --vendor MaidSafe --maintainer qa@maidsafe.net --prefix $HOME/MaidSafe --description "The Ants are coming!!" --url "http://maidsafe.net" maidsafe_vault
