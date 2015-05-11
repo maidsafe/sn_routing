947b966daa578b1728f4dca91f2a64a5566c3774
prakash <prakash@maidsafe.net>
2015-05-11 16:03:32 +0100

    wip on test challenge validation

--- src/challenge.rs
+++ src/challenge.rs
@@ -16,30 +16,34 @@
 // relating to use of the SAFE Network Software.
 
 use cbor::CborTagEncode;
+use sodiumoxide::crypto;
 use rustc_serialize::{Decodable, Decoder, Encodable, Encoder};
 use NameType;
+use types::PublicSignKey;
 
-pub struct Challenge {
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ChallengeRequest {
     pub name: NameType,  // can be my id + peer endpoint or a timestamp?
 }
 
-impl Encodable for Challenge {
+impl Encodable for ChallengeRequest {
     fn encode<E: Encoder>(&self, e: &mut E)->Result<(), E::Error> {
-        CborTagEncode::new(5483_001, &(&self.challenge)).encode(e)
+        CborTagEncode::new(5483_001, &(&self.name)).encode(e)
     }
 }
 
-impl Decodable for Challenge {
-    fn decode<D: Decoder>(d: &mut D)->Result<Challenge, D::Error> {
+impl Decodable for ChallengeRequest {
+    fn decode<D: Decoder>(d: &mut D)->Result<ChallengeRequest, D::Error> {
         try!(d.read_u64());
-        let challenge = try!(Decodable::decode(d));
-        Ok(Challenge { challenge: challenge })
+        let name = try!(Decodable::decode(d));
+        Ok(ChallengeRequest { name: name })
     }
 }
 
+#[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ChallengeResponse {
     pub name: NameType,
-    pub signature : crypto::sign::Signature,
+    pub signature: PublicSignKey,
     pub request: ChallengeRequest,
 }
 
@@ -49,28 +53,53 @@ pub fn validate(public_sign_key: &crypto::sign::PublicKey,
 }
 
 impl Encodable for ChallengeResponse {
-    fn encode<E: Encoder>(&self, e: &mut E)->Result<(), E::Error> {
-        CborTagEncode::new(5483_001, &(&self.name, &self.challenge_response)).encode(e)
+    fn encode<E: Encoder>(&self, encoder: &mut E)->Result<(), E::Error> {
+        CborTagEncode::new(5483_001, &(&self.name,
+                                       &self.signature,
+                                       &self.request,)).encode(encoder)
     }
 }
 
 impl Decodable for ChallengeResponse {
     fn decode<D: Decoder>(d: &mut D)->Result<ChallengeResponse, D::Error> {
         try!(d.read_u64());
-        let (name, challenge_response) = try!(Decodable::decode(d));
-        Ok(ChallengeResponse { name: name,  challenge_response: challenge_response})
+        let (name, signature, request) = try!(Decodable::decode(d));
+        Ok(ChallengeResponse { name: name,  signature: signature, request: request })
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::*;
     use cbor;
+    use super::*;
+    use NameType;
     use test_utils::Random;
 
     #[test]
     fn challenge_validation() {
-        let challenge : Challenge = Random::generate_random();
+        let orginal_request = ChallengeRequest{ name: Random::generate_random() };
+
+        // serialise
+        let mut encoded_request = cbor::Encoder::from_memory();
+        encoded_request.encode(&[&orginal_request]).unwrap();
+
+        // parse
+         let mut decoded_request = cbor::Decoder::from_bytes(encoded_request.as_bytes());
+         let request: ChallengeRequest = decoded_request.decode().next().unwrap().unwrap();
+         assert_eq!(orginal_request, request);
+
+         // response
+        let orginal_response = ChallengeResponse{ name: Random::generate_random(),
+                                                  signature: Random::generate_random(),
+                                                  request: request };
+        // serialise response
+        let mut encoded_response = cbor::Encoder::from_memory();
+        encoded_response.encode(&[&orginal_response]).unwrap();
+
+        // parse
+         let mut decoded_response = cbor::Decoder::from_bytes(encoded_request.as_bytes());
+         let response: ChallengeResponse = decoded_response.decode().next().unwrap().unwrap();
+         assert_eq!(orginal_response, response);
 
         // let mut e = cbor::Encoder::from_memory();
         // e.encode(&[&obj_before]).unwrap();
