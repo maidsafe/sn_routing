d595ba70f83077682fd57b0d325b1090ae43b458
Benjamin Bollen <benjamin.bollen@maidsafe.net>
2015-05-26 17:49:53 +0100

    Initial structs for non-routing table connections

--- src/relay.rs
+++ src/relay.rs
@@ -20,3 +20,37 @@
 //!
 //! As such the relay module handles messages that need to flow in or out of the SAFE network.
 //! These messages include bootstrap actions by starting nodes or relay messages for clients.
+
+use crust::Endpoint;
+use types::PublicId;
+
+const MAX_RELAY : usize = 5;
+
+#[derive(Clone, Debug)]
+pub struct IpNodeInfo {
+    pub fob: PublicId,
+    // drop IpNodeInfo upon losing connection
+    pub connected_endpoint: Endpoint
+}
+
+impl IpNodeInfo {
+    pub fn new(fob: PublicId, connected_endpoint: Endpoint) -> IpNodeInfo {
+        IpNodeInfo {
+            fob: fob,
+            connected_endpoint: connected_endpoint
+        }
+    }
+}
+
+/// The relay map is used to maintain a list of contacts for whom
+/// we are relaying messages, when we are ourselves connected to the network.
+pub struct RelayMap {
+    relay_map: Vec<IpNodeInfo>,
+    our_id: NameType
+}
+
+/// Bootstrap endpoints are used to connect to the network before
+/// routing table connections are established.
+pub struct BootstrapEndpoints {
+    bootstrap_endpoints: Vec<IpNodeInfo>,
+}
