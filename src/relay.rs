05b0d1a634c9b955f9c0834e8c212b2d738cb8bd
Benjamin Bollen <benjamin.bollen@maidsafe.net>
2015-05-28 12:09:21 +0100

    Add tests for RelayMap

--- src/relay.rs
+++ src/relay.rs
@@ -28,37 +28,21 @@ use NameType;
 
 const MAX_RELAY : usize = 5;
 
-// #[derive(Clone, Debug)]
-// pub struct IpNodeInfo {
-//     pub fob: PublicId,
-//     // drop IpNodeInfo upon losing connection
-//     pub connected_endpoint: Endpoint
-// }
-//
-// impl IpNodeInfo {
-//     pub fn new(fob: PublicId, connected_endpoint: Endpoint) -> IpNodeInfo {
-//         IpNodeInfo {
-//             fob: fob,
-//             connected_endpoint: connected_endpoint
-//         }
-//     }
-// }
-
 /// The relay map is used to maintain a list of contacts for whom
 /// we are relaying messages, when we are ourselves connected to the network.
 pub struct RelayMap {
     relay_map: BTreeMap<NameType, (PublicId, BTreeSet<Endpoint>)>,
     lookup_map: HashMap<Endpoint, NameType>,
-    our_id: NameType
+    our_name: NameType
 }
 
 impl RelayMap {
     /// This creates a new RelayMap.
-    pub fn new(our_id: NameType) -> RelayMap {
+    pub fn new(our_name: &NameType) -> RelayMap {
         RelayMap {
             relay_map: BTreeMap::new(),
-            lookup_map: HashMap::<Endpoint, NameType>::new(),
-            our_id: our_id
+            lookup_map: HashMap::new(),
+            our_name: our_name.clone()
         }
     }
 
@@ -69,17 +53,16 @@ impl RelayMap {
     /// Returns false if the endpoint is already assigned to a different name.
     pub fn add_ip_node(&mut self, relay_info: PublicId, relay_endpoint: Endpoint) -> bool {
         // always reject our own id
-        if self.our_id == relay_info.name {
+        if self.our_name == relay_info.name {
             return false;
         }
-
         // impose limit on number of relay nodes active
         if !self.relay_map.contains_key(&relay_info.name)
             && self.relay_map.len() >= MAX_RELAY {
             return false;
         }
-
-        if self.lookup_map.contains_key(&relay_endpoint) { return false; }
+        if self.lookup_map.contains_key(&relay_endpoint) {
+          return false; }
         self.lookup_map.entry(relay_endpoint.clone())
                        .or_insert(relay_info.name.clone());
         let new_set = || { (relay_info.clone(), BTreeSet::<Endpoint>::new()) };
@@ -127,16 +110,92 @@ pub struct BootstrapEndpoints {
 #[cfg(test)]
 mod test {
     use super::*;
+    use NameType;
+    use crust::Endpoint;
+    use types::{Id, PublicId};
+    use std::net::SocketAddr;
+    use std::str::FromStr;
     use test_utils::Random;
+    use rand::random;
 
     fn generate_random_endpoint() -> Endpoint {
-      
+        Endpoint::Tcp(SocketAddr::from_str(&format!("127.0.0.1:{}", random::<u16>())).unwrap())
     }
 
     #[test]
     fn add() {
-        let our_id : NameType = Random::generate_random();
-        let relay_map = RelayMap::new(our_id);
+        let our_id : Id = Id::new();
+        let our_public_id = PublicId::new(&our_id);
+        let our_name = our_id.get_name();
+        let mut relay_map = RelayMap::new(&our_name);
+        assert_eq!(false, relay_map.add_ip_node(our_public_id.clone(), generate_random_endpoint()));
+        assert_eq!(0, relay_map.relay_map.len());
+        assert_eq!(0, relay_map.lookup_map.len());
+        for _ in 0..super::MAX_RELAY {
+            assert_eq!(true, relay_map.add_ip_node(PublicId::new(&Id::new()),
+                             generate_random_endpoint()));
+        }
+        assert_eq!(false, relay_map.add_ip_node(PublicId::new(&Id::new()),
+                          generate_random_endpoint()));
+    }
 
+    #[test]
+    fn drop() {
+        let our_name : NameType = Random::generate_random();
+        let mut relay_map = RelayMap::new(&our_name);
+        let test_public_id = PublicId::new(&Id::new());
+        let test_endpoint = generate_random_endpoint();
+        assert_eq!(true, relay_map.add_ip_node(test_public_id.clone(),
+                                               test_endpoint.clone()));
+        assert_eq!(true, relay_map.contains_relay_for(&test_public_id.name));
+        assert_eq!(true, relay_map.contains_endpoint(&test_endpoint));
+        relay_map.drop_ip_node(&test_public_id.name);
+        assert_eq!(false, relay_map.contains_relay_for(&test_public_id.name));
+        assert_eq!(false, relay_map.contains_endpoint(&test_endpoint));
+        assert_eq!(None, relay_map.get_endpoints(&test_public_id.name));
+    }
+
+    #[test]
+    fn add_conflicting_endpoints() {
+        let our_name : NameType = Random::generate_random();
+        let mut relay_map = RelayMap::new(&our_name);
+        let test_public_id = PublicId::new(&Id::new());
+        let test_endpoint = generate_random_endpoint();
+        let test_conflicting_public_id = PublicId::new(&Id::new());
+        assert_eq!(true, relay_map.add_ip_node(test_public_id.clone(),
+                                               test_endpoint.clone()));
+        assert_eq!(true, relay_map.contains_relay_for(&test_public_id.name));
+        assert_eq!(true, relay_map.contains_endpoint(&test_endpoint));
+        assert_eq!(false, relay_map.add_ip_node(test_conflicting_public_id.clone(),
+                                                test_endpoint.clone()));
+        assert_eq!(false, relay_map.contains_relay_for(&test_conflicting_public_id.name))
+    }
+
+    #[test]
+    fn add_multiple_endpoints() {
+        let our_name : NameType = Random::generate_random();
+        let mut relay_map = RelayMap::new(&our_name);
+        assert!(super::MAX_RELAY - 1 > 0);
+        // ensure relay_map is all but full, so multiple endpoints are not counted as different
+        // relays.
+        for _ in 0..super::MAX_RELAY - 1 {
+            assert_eq!(true, relay_map.add_ip_node(PublicId::new(&Id::new()),
+                             generate_random_endpoint()));
+        }
+        let test_public_id = PublicId::new(&Id::new());
+        let test_endpoint_1 = generate_random_endpoint();
+        let test_endpoint_2 = generate_random_endpoint();
+        assert_eq!(true, relay_map.add_ip_node(test_public_id.clone(),
+                                               test_endpoint_1.clone()));
+        assert_eq!(true, relay_map.contains_relay_for(&test_public_id.name));
+        assert_eq!(true, relay_map.contains_endpoint(&test_endpoint_1));
+        assert_eq!(false, relay_map.add_ip_node(test_public_id.clone(),
+                                                test_endpoint_1.clone()));
+        assert_eq!(true, relay_map.add_ip_node(test_public_id.clone(),
+                                               test_endpoint_2.clone()));
+        assert!(relay_map.get_endpoints(&test_public_id.name).unwrap().1
+                         .contains(&test_endpoint_1));
+        assert!(relay_map.get_endpoints(&test_public_id.name).unwrap().1
+                         .contains(&test_endpoint_2));
     }
 }
