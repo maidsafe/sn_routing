17543d6ce5ce238f283ae2c2f83abfe83b676ddb
maqi <qi.ma@maidsafe.net>
2015-06-01 12:17:32 +0100

    add ResponseError::FailedToStoreData

--- src/types.rs
+++ src/types.rs
@@ -402,6 +402,7 @@ impl Encodable for ResponseError {
         match *self {
             ResponseError::NoData => type_tag = "NoData",
             ResponseError::InvalidRequest => type_tag = "InvalidRequest",
+            ResponseError::FailedToStoreData(_) => type_tag = "FailedToStoreData",
         };
         CborTagEncode::new(5483_100, &(&type_tag)).encode(e)
     }
