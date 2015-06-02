240c97972855c6a36db9c4dd4413151c1d81011e
maqi <qi.ma@maidsafe.net>
2015-06-02 04:49:58 +0100

    pmid_node::handle_put comment update

--- src/pmid_node/mod.rs
+++ src/pmid_node/mod.rs
@@ -88,10 +88,10 @@ impl PmidNode {
           if fetched_data.len() > required_space {
             self.chunk_store_.delete(name.clone());
             self.chunk_store_.put(data_name, data);
-            // TODO: ideally, the InterfaceError shall have an option like : 
-            //       Forward(Vec<u8>) where Vec<u8> is the data to be removed
+            // TODO: ideally, the InterfaceError shall have an option like :
+            //       ResponseError(Vec<Vec<u8>>) where Vec<u8> is the sacrificial copy got removed
             // Currently, one option is to use Reply<Vec<u8>> to indicate a removal
-            // and let routing compose proper notification
+            // and let routing compose proper notification. This only supports removing one sacrificial copy each time.
             return Ok(MessageAction::Reply(fetched_data));
             // Another option is Routing will send upon error a put_data_response message back to the previous PM
             // and then PMs can read the error and MessageAction::SendOn(to DM). However size info has to be skipped
