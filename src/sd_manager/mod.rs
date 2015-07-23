c052dc4ab9e5146d63a74b0c597a6377cf6982ab
maqi <qi.ma@maidsafe.net>
2015-07-23 16:19:35 +0100

    sd_manager::handle_post test

--- src/sd_manager/mod.rs
+++ src/sd_manager/mod.rs
@@ -114,10 +114,13 @@ impl StructuredDataManager {
 
 #[cfg(test)]
 mod test {
+    use sodiumoxide::crypto;
+
     use super::*;
     use utils::encode;
 
     use routing::data::Data;
+    use routing::error::ResponseError;
     use routing::NameType;
     use routing::node_interface::MethodCall;
     use routing::sendable::Sendable;
@@ -164,6 +167,76 @@ mod test {
                         _ => panic!("Unexpected"),
                     }
                 }
+                _ => panic!("Unexpected"),
+            }
+        }
+    }
+
+    #[test]
+    fn handle_post() {
+        let mut sd_manager = StructuredDataManager::new();
+        let name = NameType([3u8; 64]);
+        let value = generate_random_vec_u8(1024);
+        let sdv = StructuredData::new(0, name, value.clone(), vec![], 0, vec![], vec![]);
+        { // posting to none existing data
+            match sd_manager.handle_post(sdv.clone()) {
+                Err(result) => { assert_eq!(result, From::from(ResponseError::NoData)); }
+                _ => panic!("Unexpected"),
+            }
+        }
+        {
+            let put_result = sd_manager.handle_put(sdv.clone());
+            assert_eq!(put_result.is_ok(), true);
+            let mut calls = put_result.ok().unwrap();
+            assert_eq!(calls.len(), 1);
+            match calls.remove(0) {
+                MethodCall::Reply { data } => {
+                    match data {
+                        Data::StructuredData(sd) => {
+                            assert_eq!(sd, sdv);
+                        }
+                        _ => panic!("Unexpected"),
+                    }
+                }
+                _ => panic!("Unexpected"),
+            }
+        }
+        { // incorrect version
+            let sdv_new = StructuredData::new(0, name, value.clone(), vec![], 3, vec![], vec![]);
+            match sd_manager.handle_post(sdv_new.clone()) {
+                Err(result) => { assert_eq!(result, From::from(ResponseError::InvalidRequest)); }
+                _ => panic!("Unexpected"),
+            }
+        }
+        { // correct version non-owner
+            let sdv_new = StructuredData::new(0, name, value.clone(), vec![], 1, vec![], vec![]);
+            match sd_manager.handle_post(sdv_new.clone()) {
+                Ok(_) => {}
+                _ => panic!("Unexpected"),
+            }
+        }
+        let keys1 = crypto::sign::gen_keypair();
+        { // update to having an owner
+            let sdv_new = StructuredData::new(0, name, value.clone(), vec![], 2, vec![keys1.0], vec![]);
+            match sd_manager.handle_post(sdv_new.clone()) {
+                Ok(_) => {}
+                _ => panic!("Unexpected"),
+            }
+        }
+        let keys2 = crypto::sign::gen_keypair();
+        { // update to a new owner, wrong signature
+            let mut sdv_new = StructuredData::new(0, name, value.clone(), vec![keys1.0], 3, vec![keys2.0], vec![]);
+            assert_eq!(sdv_new.add_signature(&keys2.1).ok(), Some(0));
+            match sd_manager.handle_post(sdv_new.clone()) {
+                Err(result) => { assert_eq!(result, From::from(ResponseError::InvalidRequest)); }
+                _ => panic!("Unexpected"),
+            }
+        }
+        { // update to a new owner, correct signatur
+            let mut sdv_new = StructuredData::new(0, name, value.clone(), vec![keys1.0], 3, vec![keys2.0], vec![]);
+            assert_eq!(sdv_new.add_signature(&keys1.1).ok(), Some(0));
+            match sd_manager.handle_post(sdv_new.clone()) {
+                Ok(_) => {}
                 _ => panic!("Unexpected"),
             }
         }
