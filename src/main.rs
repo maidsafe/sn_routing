5f47643db8c7baac307a9436ad87fffbcb1fe821
maqi <qi.ma@maidsafe.net>
2015-07-27 10:13:45 +0100

    routing_node having run_zero_membrane and bootstrap merged into run function

--- src/main.rs
+++ src/main.rs
@@ -34,11 +34,9 @@
 //!
 //! The resulting executable is the Vault node for the SAFE network.
 //! Refer to https://github.com/dirvine/maidsafe_vault
-#![feature(convert, core)]
+#![feature(core)]
 
 extern crate core;
-extern crate crust;
-extern crate docopt;
 extern crate rustc_serialize;
 extern crate cbor;
 extern crate time;
@@ -50,15 +48,8 @@ extern crate rand;
 #[cfg(test)]
 extern crate maidsafe_sodiumoxide as sodiumoxide;
 
-use core::iter::FromIterator;
-use std::net::SocketAddr;
-use std::str::FromStr;
 use std::thread;
 use std::thread::spawn;
-
-
-use docopt::Docopt;
-use rustc_serialize::{Decodable, Decoder};
 
 mod data_manager;
 mod maid_manager;
@@ -70,7 +61,6 @@ mod transfer_parser;
 mod vault;
 mod utils;
 
-use crust::Endpoint;
 use vault::{ VaultFacade, VaultGenerator };
 
 /// Placeholder doc test
@@ -89,71 +79,14 @@ impl Vault {
     }
 }
 
-// ==========================   Program Options   =================================
-static USAGE: &'static str = "
-Usage:
-  maidsafe_vault [<peer>...]
-  maidsafe_vault --first
-  maidsafe_vault --help
-
-Options:
-  -f, --first  Node runs as the first vault in the network.
-  -h, --help   Display this help message.
-
-  Running without '--first' requires an existing network to connect to.  If this
-  is the first vault of a new network, the only arg passed should be '--first'.
-
-  The optional <peer>... arg(s) are a list of peer endpoints (other running vaults).
-  If these are supplied, the node will try to connect to one of these in order to
-  join the network.  If no endpoints are supplied, the node will try to connect to
-  an existing network using Crust's discovery protocol.
-";
-
-#[derive(RustcDecodable, Debug)]
-struct Args {
-    arg_peer: Vec<PeerEndpoint>,
-    flag_first: bool,
-    flag_help: bool,
-}
-
-#[derive(Debug)]
-enum PeerEndpoint {
-    Tcp(SocketAddr),
-}
-
-impl Decodable for PeerEndpoint {
-    fn decode<D: Decoder>(decoder: &mut D)->Result<PeerEndpoint, D::Error> {
-        let str = try!(decoder.read_str());
-        let address = match SocketAddr::from_str(&str) {
-            Ok(addr) => addr,
-            Err(_) => {
-                return Err(decoder.error(format!(
-                    "Could not decode {} as valid IPv4 or IPv6 address.", str).as_str()));
-            },
-        };
-        Ok(PeerEndpoint::Tcp(address))
-    }
-}
-
 /// Main entry for start up a vault node
 pub fn main () {
-    let args: Args = Docopt::new(USAGE)
-                            .and_then(|docopt| docopt.decode())
-                            .unwrap_or_else(|error| error.exit());
-    // Convert peer endpoints to usable bootstrap list.
-    let bootstrap_peers = if args.arg_peer.is_empty() {
-        None
-    } else {
-        Some(Vec::<Endpoint>::from_iter(args.arg_peer.iter().map(|endpoint| {
-            Endpoint::Tcp(match *endpoint { PeerEndpoint::Tcp(address) => address, })
-        })))
-    };
-
+    // routing changed to eliminate the difference of the first and later on nodes on network
+    // the routing_node.run() replaces the previous run_zero_membrance() and bootstrap() function
     let mut vault = Vault::new();
-    if args.flag_first {
-        vault.routing_node.run_zero_membrane();
-    } else {
-        let _ = vault.routing_node.bootstrap(bootstrap_peers);
+    match vault.routing_node.run() {
+        Err(err) => panic!("Could not connect to the network with error : {:?}", err),
+        _ => {}
     }
     let thread_guard = spawn(move || {
         loop {
@@ -162,6 +95,7 @@ pub fn main () {
     });
     let _ = thread_guard.join();
 }
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -172,7 +106,10 @@ mod test {
     fn lib_test() {
         let run_vault = |mut vault: Vault| {
             spawn(move || {
-                let _ = vault.routing_node.bootstrap(None);
+                match vault.routing_node.run() {
+                    Err(err) => panic!("Could not connect to the network with error : {:?}", err),
+                    _ => {}
+                }
                 let thread_guard = spawn(move || {
                     loop {
                         thread::sleep_ms(1);
@@ -185,18 +122,7 @@ mod test {
         // The stable duration needs to be increased dramatically along with the increase of the total node numbers.
         // for example, you may need i * 1500 when increase total nodes from 8 to 9
         // The first node must be run in membrane mode
-        let _ = spawn(move || {
-                let mut vault = Vault::new();
-                let _ = vault.routing_node.run_zero_membrane();
-                let thread_guard = spawn(move || {
-                    loop {
-                        thread::sleep_ms(1);
-                    }
-                });
-                let _ = thread_guard.join();
-            });
-        thread::sleep_ms(1000);
-        for i in 1..8 {
+        for i in 0..8 {
             let _ = run_vault(Vault::new());
             thread::sleep_ms(1000 + i * 1000);
         }
