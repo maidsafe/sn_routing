613683da58306bf35e440011d0b525177ea38e8e
maqi <qi.ma@maidsafe.net>
2015-08-18 08:04:18 +0100

    make using new_routing data related types passing tests

--- src/sd_manager/mod.rs
+++ src/sd_manager/mod.rs
@@ -120,7 +120,8 @@ mod test {
         let mut sd_manager = StructuredDataManager::new();
         let name = NameType([3u8; 64]);
         let value = generate_random_vec_u8(1024);
-        let sdv = StructuredData::new(0, name, value.clone(), vec![], 0, vec![], vec![]);
+        let keys = crypto::sign::gen_keypair();
+        let sdv = StructuredData::new(0, name, 0, value.clone(), vec![keys.0], vec![], &keys.1).ok().unwrap();
         {
             let put_result = sd_manager.handle_put(sdv.clone());
             assert_eq!(put_result.is_ok(), true);
@@ -165,7 +166,8 @@ mod test {
         let mut sd_manager = StructuredDataManager::new();
         let name = NameType([3u8; 64]);
         let value = generate_random_vec_u8(1024);
-        let sdv = StructuredData::new(0, name, value.clone(), vec![], 0, vec![], vec![]);
+        let keys = crypto::sign::gen_keypair();
+        let sdv = StructuredData::new(0, name, 0, value.clone(), vec![keys.0], vec![], &keys.1).ok().unwrap();
         { // posting to none existing data
             match sd_manager.handle_post(sdv.clone()) {
                 Err(result) => { assert_eq!(result, From::from(ResponseError::NoData)); }
@@ -190,22 +192,14 @@ mod test {
             }
         }
         { // incorrect version
-            let sdv_new = StructuredData::new(0, name, value.clone(), vec![], 3, vec![], vec![]);
+            let sdv_new = StructuredData::new(0, name, 3, value.clone(), vec![keys.0], vec![], &keys.1).ok().unwrap();
             match sd_manager.handle_post(sdv_new.clone()) {
                 Err(result) => { assert_eq!(result, From::from(ResponseError::InvalidRequest)); }
                 _ => panic!("Unexpected"),
             }
         }
-        { // correct version non-owner
-            let sdv_new = StructuredData::new(0, name, value.clone(), vec![], 1, vec![], vec![]);
-            match sd_manager.handle_post(sdv_new.clone()) {
-                Ok(_) => {}
-                _ => panic!("Unexpected"),
-            }
-        }
-        let keys1 = crypto::sign::gen_keypair();
-        { // update to having an owner
-            let sdv_new = StructuredData::new(0, name, value.clone(), vec![], 2, vec![keys1.0], vec![]);
+        { // correct version
+            let sdv_new = StructuredData::new(0, name, 1, value.clone(), vec![keys.0], vec![], &keys.1).ok().unwrap();
             match sd_manager.handle_post(sdv_new.clone()) {
                 Ok(_) => {}
                 _ => panic!("Unexpected"),
@@ -213,16 +207,14 @@ mod test {
         }
         let keys2 = crypto::sign::gen_keypair();
         { // update to a new owner, wrong signature
-            let mut sdv_new = StructuredData::new(0, name, value.clone(), vec![keys1.0], 3, vec![keys2.0], vec![]);
-            assert_eq!(sdv_new.add_signature(&keys2.1).ok(), Some(0));
+            let mut sdv_new = StructuredData::new(0, name, 2, value.clone(), vec![keys2.0], vec![keys.0], &keys2.1).ok().unwrap();
             match sd_manager.handle_post(sdv_new.clone()) {
                 Err(result) => { assert_eq!(result, From::from(ResponseError::InvalidRequest)); }
                 _ => panic!("Unexpected"),
             }
         }
-        { // update to a new owner, correct signatur
-            let mut sdv_new = StructuredData::new(0, name, value.clone(), vec![keys1.0], 3, vec![keys2.0], vec![]);
-            assert_eq!(sdv_new.add_signature(&keys1.1).ok(), Some(0));
+        { // update to a new owner, correct signature
+            let mut sdv_new = StructuredData::new(0, name, 2, value.clone(), vec![keys2.0], vec![keys.0], &keys.1).ok().unwrap();
             match sd_manager.handle_post(sdv_new.clone()) {
                 Ok(_) => {}
                 _ => panic!("Unexpected"),
@@ -234,7 +226,8 @@ mod test {
     fn handle_account_transfer() {
         let name = NameType([3u8; 64]);
         let value = generate_random_vec_u8(1024);
-        let sdv = StructuredData::new(0, name, value, vec![], 0, vec![], vec![]);
+        let keys = crypto::sign::gen_keypair();
+        let sdv = StructuredData::new(0, name, 0, value, vec![keys.0], vec![], &keys.1).ok().unwrap();
 
         let mut sd_manager = StructuredDataManager::new();
         let serialised_data = match encode(&sdv) {
