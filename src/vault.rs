16886c5d5affcf3fe13a20e50bfed1eb3b0a2d3a
maqi <qi.ma@maidsafe.net>
2015-08-27 09:03:31 +0100

    re-enable executable_test

--- src/vault.rs
+++ src/vault.rs
@@ -172,6 +172,9 @@ impl Vault {
     }
 
     fn on_churn(&mut self, close_group: Vec<::routing::NameType>) {
+        if close_group.len() > self.nodes_in_table.len() {
+            info!("vault added connected node");
+        }
         let refresh_calls = self.handle_churn(close_group);
         self.send(Authority::NaeManager(NameType::new([0u8; 64])),
                   refresh_calls, None, None, None);
