9b0b4ddf70169581e3b5b1b9606c0b285e0d542c
David Irvine <david.irvine@maidsafe.net>
2015-09-03 00:31:27 +0100

    Simplify unknown_endpoints and time them out dropping after 10 minutes unless removed on receipt of a Hello (move to client connections) or ConnectResponse (move to routing_table, maybe)

--- src/routing_core.rs
+++ src/routing_core.rs
@@ -20,6 +20,7 @@ use std::sync::mpsc::Sender;
 
 use crust;
 
+use std::collections::HashMap;
 use routing_table::{RoutingTable, NodeInfo};
 use relay::RelayMap;
 use types::Address;
@@ -32,6 +33,8 @@ use peer::Peer;
 use action::Action;
 use event::Event;
 use messages::RoutingMessage;
+use time::{Duration, SteadyTime};
+use crust::{Endpoint};
 
 /// ConnectionName labels the counterparty on a connection in relation to us
 #[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Clone)]
@@ -51,6 +54,7 @@ pub struct RoutingCore {
     network_name: Option<NameType>,
     routing_table: Option<RoutingTable>,
     relay_map: RelayMap,
+    unknown_connection_map: HashMap<Endpoint, SteadyTime>,
     // sender for signaling events and action
     event_sender: Sender<Event>,
     action_sender: Sender<Action>,
@@ -78,6 +82,7 @@ impl RoutingCore {
             network_name: None,
             routing_table: None,
             relay_map: RelayMap::new(),
+            unknown_connection_map: HashMap::new(),
             event_sender: event_sender,
             action_sender: action_sender,
         }
@@ -241,6 +246,23 @@ impl RoutingCore {
                 dropped_peer
             }
         }
+    }
+
+    /// Add all connections here first, until we can match them with either a 
+    /// HelloResponse or ConnectResponse
+    pub fn add_unknown_endpoint(&mut self, endpoint : Endpoint)-> Option<Vec<Endpoint>> {
+        self.unknown_connection_map.insert(endpoint, SteadyTime::now());    
+        self.clear_old_endpoints()
+     }
+
+    fn clear_old_endpoints(&mut self)-> Option<Vec<Endpoint>> {
+        let timed_out: Vec<_> = self.unknown_connection_map
+         .iter()
+         .filter(|&(_, &v)| v > SteadyTime::now() + Duration::minutes(10))
+         .map(|(k, _)| k.clone())
+         .collect();
+        for old in &timed_out { self.unknown_connection_map.remove(old); }    
+        if timed_out.is_empty() { None } else { Some(timed_out.clone()) }
     }
 
     /// To be documented
