b0311f6b8e2d6c317734e2a78d33ebd54e04aed1
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2015-10-07 16:46:58 +0100

    Minor installer changes.

---
+++ installer/linux/test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+id safe
+ls -la /usr/bin/*safe*
+ls -la /var/cache/safe_vault/
+ls -la /var/log/safe-vault.log
+ls -la /var/log/safe-vault.err
+echo "safe_vault PID: $(pgrep safe_vault)"
---
+++ installer/osx/test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+id safe
+ls -la /usr/local/bin/*safe*
+ls -la /var/cache/safe_vault/
+ls -la /Library/LaunchDaemons/org.maidsafe*
+ls -la /var/log/safe/safe_vault.log
+ls -la /var/log/safe/safe_vault.err
+sudo launchctl list org.maidsafe.safe_vault
