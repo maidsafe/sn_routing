280c611098fa66d5dafc3280adab1c4dea09ef43
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2016-03-02 14:55:48 +0000

    Updated to match changes in Routing.

--- src/personas/mpid_manager.rs
+++ src/personas/mpid_manager.rs
@@ -627,6 +627,7 @@ mod test {
             our_authority: Authority::ClientManager(from.clone()),
             client: Authority::Client {
                 client_key: keys.0,
+                peer_id: rand::random(),
                 proxy_node_name: from.clone(),
             },
             routing: unwrap_result!(RoutingNode::new(mpsc::channel().0)),
@@ -655,6 +656,7 @@ mod test {
     fn generate_receiver() -> Authority {
         Authority::Client {
             client_key: sign::gen_keypair().0,
+            peer_id: rand::random(),
             proxy_node_name: rand::random::<XorName>(),
         }
     }
