1e3c0811d1cdbc752feb681b7302a16b5b59431e
maqi <qi.ma@maidsafe.net>
2016-03-03 14:03:04 +0000

    pmid_noding sending responses for put request

--- src/vault.rs
+++ src/vault.rs
@@ -192,7 +192,7 @@ impl Vault {
             }
             (&Authority::NodeManager(_),
              &Authority::ManagedNode(_),
-             &RequestContent::Put(Data::Immutable(_), _)) => self.pmid_node.handle_put(&request),
+             &RequestContent::Put(Data::Immutable(_), _)) => self.pmid_node.handle_put(routing_node, &request),
             // ================== Post ==================
             (&Authority::Client{ .. },
              &Authority::NaeManager(_),
