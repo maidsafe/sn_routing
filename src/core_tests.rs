4e67e456c23300b842de05869132542afa8e7484
brian-js <brian.smith@maidsafe.net>
2016-03-16 22:05:09 +0000

    Initial message test.

--- src/core_tests.rs
+++ src/core_tests.rs
@@ -15,15 +15,23 @@
 // Please review the Licences for the specific language governing permissions and limitations
 // relating to use of the SAFE Network Software.
 
+use rand::{self, Rng};
 use std::cmp;
 use std::collections::HashSet;
 use std::sync::mpsc;
 use xor_name::XorName;
 
+use action::Action;
+use authority::Authority;
 use core::{Core, RoutingTable};
+use data::{Data, ImmutableData, ImmutableDataType};
+use error::InterfaceError;
 use event::Event;
+use id::FullId;
 use kademlia_routing_table::{ContactInfo, GROUP_SIZE};
+use messages::{RequestContent, RequestMessage};
 use mock_crust::{self, Config, Endpoint, Network, ServiceHandle};
+use types::{MessageId, RoutingActionSender};
 
 struct TestNode {
     handle: ServiceHandle,
@@ -70,6 +78,67 @@ impl TestNode {
 
     fn routing_table(&self) -> &RoutingTable {
         self.core.routing_table()
+    }
+}
+
+struct TestClient {
+    _handle: ServiceHandle,
+    core: Core,
+    event_rx: mpsc::Receiver<Event>,
+    action_tx: RoutingActionSender,
+}
+
+impl TestClient {
+    fn new(network: &Network,
+           config: Option<Config>,
+           endpoint: Option<Endpoint>)
+           -> Self {
+        let handle = network.new_service_handle(config, endpoint);
+        let (event_tx, event_rx) = mpsc::channel();
+        let full_id = FullId::new();
+
+        let (action_tx, core) = mock_crust::make_current(&handle, || Core::new(event_tx, true, Some(full_id)));
+
+        TestClient {
+            _handle: handle,
+            core: core,
+            event_rx: event_rx,
+            action_tx: action_tx,
+        }
+    }
+
+    fn poll(&mut self) -> bool {
+        let mut result = false;
+
+        while self.core.poll() {
+            result = true;
+        }
+
+        result
+    }
+
+    fn name(&self) -> &XorName {
+        self.core.name()
+    }
+
+    fn send_put_request(&self,
+                        dst: Authority,
+                        data: Data,
+                        message_id: MessageId)
+                        -> Result<(), InterfaceError> {
+        self.send_action(RequestContent::Put(data, message_id), dst)
+    }
+
+    fn send_action(&self, content: RequestContent, dst: Authority) -> Result<(), InterfaceError> {
+        let (interface_tx, interface_rx) = mpsc::channel();
+        let action = Action::ClientSendRequest {
+            content: content,
+            dst: dst,
+            result_tx: interface_tx.clone(),
+        };
+
+        try!(self.action_tx.send(action));
+        try!(interface_rx.recv())
     }
 }
 
@@ -255,4 +324,53 @@ fn node_drops() {
     drop_node(&mut nodes, 0);
 
     verify_kademlia_invariant_for_all_nodes(&nodes);
+}
+
+#[test]
+fn send_put_request() {
+    let network = Network::new();
+    let mut nodes = Vec::new();
+
+    nodes.push(TestNode::new(&network, false, None, Some(Endpoint(0))));
+    nodes[0].poll();
+
+    let config = Config::with_contacts(&[nodes[0].handle.endpoint()]);
+
+    for i in 1..GROUP_SIZE + 2 {
+        nodes.push(TestNode::new(&network, false, Some(config.clone()), Some(Endpoint(i))));
+        poll_all(&mut nodes);
+    }
+
+    assert_eq!(GROUP_SIZE + 2, nodes.len());
+
+    for node in nodes.iter() {
+        for _ in 0..GROUP_SIZE - 1 {
+            expect_event!(node, Event::NodeAdded(..))
+        }
+    }
+
+    let mut client = TestClient::new(&network, Some(config.clone()), Some(Endpoint(GROUP_SIZE + 2)));
+
+    poll_all(&mut nodes);
+    client.poll();
+    expect_event!(client, Event::Connected);
+
+    let dst = Authority::ClientManager(client.name().clone());
+    let data = rand::thread_rng().gen_iter().take(1024).collect();
+    let immutable_data = ImmutableData::new(ImmutableDataType::Normal, data);
+    let message_id = MessageId::new();
+
+    assert!(client.send_put_request(dst, Data::Immutable(immutable_data), message_id).is_ok());
+    poll_all(&mut nodes);
+
+    for node in nodes.iter().filter(|n| n.routing_table().is_close(client.name())) {
+        loop {
+            match node.event_rx.try_recv() {
+                Ok(Event::Request(RequestMessage { content: RequestContent::Put(ref data, ref id), .. })) =>
+                    if let Data::Immutable(_) = *data { if *id == message_id { break; } },
+                Ok(_) => (),
+                _ => panic!("Event::Request(..) not received"),
+            }
+        }
+    }
 }
