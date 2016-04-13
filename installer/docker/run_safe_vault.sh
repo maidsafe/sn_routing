a1c7dcfb638ffcaf1dbed7feff257bcac2378258
Adam Cigánek <adam.ciganek@gmail.com>
2016-04-13 16:54:54 +0200

    Docker image now accepts vault config option via env variables

---
+++ installer/docker/run_safe_vault.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+BIN_DIR=/root/.cargo/bin/
+
+function generate_config() {
+  local path=$1
+
+  if [ -n "$VAULT_WALLET_ADDRESS" ]; then
+    local wallet_address="\"$VAULT_WALLET_ADDRESS\""
+  else
+    local wallet_address=null
+  fi
+
+  local max_capacity=${VAULT_MAX_CAPACITY:-null}
+
+  rm -f $path
+  echo "{"                                      >> $path
+  echo "  \"wallet_address\": $wallet_address," >> $path
+  echo "  \"max_capacity\": $max_capacity"      >> $path
+  echo "}"                                      >> $path
+}
+
+generate_config $BIN_DIR/safe_vault.vault.config
+$BIN_DIR/safe_vault
