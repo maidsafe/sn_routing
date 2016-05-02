0b17f55ffffebab66fa3dec10e95f5d5157e04df
Adam Cigánek <adam.ciganek@gmail.com>
2016-05-02 13:45:16 +0200

    Add listening ports to the crust config for the docker image

--- ci/travis/deploy_docker_image.sh
+++ ci/travis/deploy_docker_image.sh
@@ -5,9 +5,19 @@ set -ev
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
 BUILD_DIR=installer/docker
+CRUST_CONFIG=$BUILD_DIR/$PROJECT_NAME.crust.config
 
 cp target/$TARGET/release/$PROJECT_NAME $BUILD_DIR
 cp -r installer/bundle/* $BUILD_DIR
+
+# Define listening ports:
+cat $CRUST_CONFIG \
+  | sed 's/\("tcp_acceptor_port":\).*\([,$]\)/\1 5000\2/' \
+  | sed 's/\("utp_acceptor_port":\).*\([,$]\)/\1 5000\2/' \
+  | sed 's/\("service_discovery_port":\).*\([,$]\)/\1 5100\2/' \
+  > $CRUST_CONFIG.new
+
+mv -f $CRUST_CONFIG.new $CRUST_CONFIG
 
 pushd $BUILD_DIR
 docker build -t $DOCKER_IMAGE:$PROJECT_VERSION .
