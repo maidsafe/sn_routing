85fb34dbca35f64c85bbef01f76bab0fc4dcd797
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2016-05-10 18:52:41 +0100

    chore/docs: clean the build script (#467)
    This removes several configuration lines, bringing the build script in line with the equivalent one
    in Routing.

--- build.rs
+++ build.rs
@@ -27,53 +27,48 @@
 extern crate hyper;
 
 #[cfg(feature = "generate-diagrams")]
-use hyper::Client;
-#[cfg(feature = "generate-diagrams")]
-use hyper::client::IntoUrl;
-#[cfg(feature = "generate-diagrams")]
-use std::fs::{self, File};
-#[cfg(feature = "generate-diagrams")]
-use std::io;
-#[cfg(feature = "generate-diagrams")]
-use std::path::{Path, PathBuf};
+mod generate_diagrams {
+    use hyper::Client;
+    use hyper::client::IntoUrl;
+    use std::fs::{self, File};
+    use std::io;
+    use std::path::{Path, PathBuf};
+
+    pub fn download_image<U: IntoUrl>(name: &str, src: U) {
+        download(src, image_path(name))
+    }
+
+    fn download<U: IntoUrl, P: AsRef<Path>>(src: U, dst: P) {
+        let client = Client::new();
+        let mut res = client.get(src).send().unwrap();
+
+        if let Some(dir) = dst.as_ref().parent() {
+            fs::create_dir_all(dir).unwrap();
+        }
+
+        let mut file = File::create(dst).unwrap();
+        io::copy(&mut res, &mut file).unwrap();
+    }
+
+    fn image_path(name: &str) -> PathBuf {
+        let mut path = PathBuf::from("target/doc/safe_vault");
+        path.push(name);
+        path.set_extension("png");
+        path
+    }
+}
 
 // Only generate the diagrams when "generate-diagrams" feature is enabled.
 // TODO: instead of this feature, detect that cargo is run in the "doc" profile.
 #[cfg(feature = "generate-diagrams")]
 fn main() {
     // List all diagram names and URLs to download them from.
-    download_image("personas",
-                   "https://cacoo.com/diagrams/wl6of3FUFriB0FWO-0BD19.png");
-    download_image("immutable-data-put-flow",
-                   "https://cacoo.com/diagrams/SCHrwEhLRB86EGe1-EF9A0.png");
-    download_image("immutable-data-get-flow",
-                   "https://cacoo.com/diagrams/ndcPMKC3WapABSaA-EF9A0.png");
-}
-
-#[cfg(feature = "generate-diagrams")]
-fn download_image<U: IntoUrl>(name: &str, src: U) {
-    download(src, image_path(name))
-}
-
-#[cfg(feature = "generate-diagrams")]
-fn download<U: IntoUrl, P: AsRef<Path>>(src: U, dst: P) {
-    let client = Client::new();
-    let mut res = client.get(src).send().unwrap();
-
-    if let Some(dir) = dst.as_ref().parent() {
-        fs::create_dir_all(dir).unwrap();
-    }
-
-    let mut file = File::create(dst).unwrap();
-    io::copy(&mut res, &mut file).unwrap();
-}
-
-#[cfg(feature = "generate-diagrams")]
-fn image_path(name: &str) -> PathBuf {
-    let mut path = PathBuf::from("target/doc/safe_vault");
-    path.push(name);
-    path.set_extension("png");
-    path
+    generate_diagrams::download_image("personas",
+                                      "https://cacoo.com/diagrams/wl6of3FUFriB0FWO-0BD19.png");
+    generate_diagrams::download_image("immutable-data-put-flow",
+                                      "https://cacoo.com/diagrams/SCHrwEhLRB86EGe1-EF9A0.png");
+    generate_diagrams::download_image("immutable-data-get-flow",
+                                      "https://cacoo.com/diagrams/ndcPMKC3WapABSaA-EF9A0.png");
 }
 
 #[cfg(not(feature = "generate-diagrams"))]
