e87bb55e7c354e936ffd1d2734a4352215d416e8
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2016-05-10 15:37:46 +0100

    fix/docs: generate the sequence diagrams
    This updates the build process to generate the docs if `--features generate-diagrams` is passed.

--- build.rs
+++ build.rs
@@ -23,50 +23,63 @@
 
 #![allow(unused)]
 
+#[cfg(feature = "generate-diagrams")]
 extern crate hyper;
 
+#[cfg(feature = "generate-diagrams")]
 use hyper::Client;
+#[cfg(feature = "generate-diagrams")]
 use hyper::client::IntoUrl;
+#[cfg(feature = "generate-diagrams")]
 use std::env;
+#[cfg(feature = "generate-diagrams")]
 use std::fs::{self, File};
+#[cfg(feature = "generate-diagrams")]
 use std::io;
+#[cfg(feature = "generate-diagrams")]
 use std::path::{Path, PathBuf};
 
+// Only generate the diagrams when "generate-diagrams" feature is enabled.
+// TODO: instead of this feature, detect that cargo is run in the "doc" profile.
+#[cfg(feature = "generate-diagrams")]
 fn main() {
-  // Only generate the diagrams when "generate-diagrams" feature is enabled.
-  // TODO: instead of this feature, detect that cargo is run in the "doc"
-  // profile.
-  if env::var("CARGO_FEATURE_GENERATE_DIAGRAMS").is_err() {
-    return;
-  }
-
-  // List all diagrams names and URLs to download them from.
-  download_image("bootstrap",       "https://cacoo.com/diagrams/cqX6QPN90ZuKXZ0n-F56A2.png");
-  download_image("get-close-group", "https://cacoo.com/diagrams/PTBt1OgHVcdu0PKt-F56A2.png");
-  download_image("new-node",        "https://cacoo.com/diagrams/5VCFe286q4yfQ6Pm-F56A2.png");
-  download_image("tunnel",          "https://cacoo.com/diagrams/ALXyW2ugR92IxgWd-F56A2.png");
+    // List all diagrams names and URLs to download them from.
+    download_image("bootstrap",
+                   "https://cacoo.com/diagrams/cqX6QPN90ZuKXZ0n-F56A2.png");
+    download_image("get-close-group",
+                   "https://cacoo.com/diagrams/PTBt1OgHVcdu0PKt-F56A2.png");
+    download_image("new-node",
+                   "https://cacoo.com/diagrams/5VCFe286q4yfQ6Pm-F56A2.png");
+    download_image("tunnel",
+                   "https://cacoo.com/diagrams/ALXyW2ugR92IxgWd-F56A2.png");
 }
 
+#[cfg(feature = "generate-diagrams")]
 fn download_image<U: IntoUrl>(name: &str, src: U) {
-  download(src, image_path(name))
+    download(src, image_path(name))
 }
 
+#[cfg(feature = "generate-diagrams")]
 fn download<U: IntoUrl, P: AsRef<Path>>(src: U, dst: P) {
-  let client = Client::new();
-  let mut res = client.get(src).send().unwrap();
+    let client = Client::new();
+    let mut res = client.get(src).send().unwrap();
 
-  if let Some(dir) = dst.as_ref().parent() {
-    fs::create_dir_all(dir).unwrap();
-  }
+    if let Some(dir) = dst.as_ref().parent() {
+        fs::create_dir_all(dir).unwrap();
+    }
 
-  let mut file = File::create(dst).unwrap();
+    let mut file = File::create(dst).unwrap();
 
-  io::copy(&mut res, &mut file).unwrap();
+    io::copy(&mut res, &mut file).unwrap();
 }
 
+#[cfg(feature = "generate-diagrams")]
 fn image_path(name: &str) -> PathBuf {
-  let mut path = PathBuf::from("target/doc/routing");
-  path.push(name);
-  path.set_extension("png");
-  path
+    let mut path = PathBuf::from("target/doc/routing");
+    path.push(name);
+    path.set_extension("png");
+    path
 }
+
+#[cfg(not(feature = "generate-diagrams"))]
+fn main() {}
