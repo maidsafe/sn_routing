41f3ac7258f8053b646d902a40ddcb50903f7363
Andreas Fackler <andreas.fackler@maidsafe.net>
2016-10-19 11:35:38 +0200

    Update Contributor Agreement to version 1.1

--- src/plain_data.rs
+++ src/plain_data.rs
@@ -5,7 +5,7 @@
 // licence you accepted on initial access to the Software (the "Licences").
 //
 // By contributing code to the SAFE Network Software, or to this project generally, you agree to be
-// bound by the terms of the MaidSafe Contributor Agreement, version 1.0.  This, along with the
+// bound by the terms of the MaidSafe Contributor Agreement, version 1.1.  This, along with the
 // Licenses can be found in the root directory of this project at LICENSE, COPYING and CONTRIBUTOR.
 //
 // Unless required by applicable law or agreed to in writing, the SAFE Network Software distributed
