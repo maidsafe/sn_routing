362013d58ca7d601f8f7bd2458aa080bdff7f4bb
Diggory Hardy <git@dhardy.name>
2016-11-15 09:54:41 +0000

    tests/lints: enable standard lints in mock_crust_tests

--- tests/mock_crust_tests.rs
+++ tests/mock_crust_tests.rs
@@ -17,6 +17,26 @@
 
 #![cfg(feature = "use-mock-crust")]
 
+// For explanation of lint checks, run `rustc -W help` or see
+// https://github.com/maidsafe/QA/blob/master/Documentation/Rust%20Lint%20Checks.md
+#![forbid(bad_style, exceeding_bitshifts, mutable_transmutes, no_mangle_const_items,
+          unknown_crate_types, warnings)]
+#![deny(deprecated, improper_ctypes, missing_docs,
+        non_shorthand_field_patterns, overflowing_literals, plugin_as_library,
+        private_no_mangle_fns, private_no_mangle_statics, stable_features, unconditional_recursion,
+        unknown_lints, unsafe_code, unused, unused_allocation, unused_attributes,
+        unused_comparisons, unused_features, unused_parens, while_true)]
+#![warn(trivial_casts, trivial_numeric_casts, unused_extern_crates, unused_import_braces,
+        unused_qualifications, unused_results)]
+#![allow(box_pointers, fat_ptr_transmutes, missing_copy_implementations,
+         missing_debug_implementations, variant_size_differences)]
+
+#![cfg_attr(feature="clippy", feature(plugin))]
+#![cfg_attr(feature="clippy", plugin(clippy))]
+#![cfg_attr(feature="clippy", deny(clippy, unicode_not_nfc, wrong_pub_self_convention,
+                                   option_unwrap_used))]
+#![cfg_attr(feature="clippy", allow(use_debug))]
+
 extern crate itertools;
 #[macro_use]
 extern crate log;
@@ -24,7 +44,6 @@ extern crate rand;
 extern crate routing;
 #[macro_use]
 extern crate unwrap;
-extern crate maidsafe_utilities;
 
 // This module is a driver and defines macros. See `mock_crust` modules for
 // tests.
