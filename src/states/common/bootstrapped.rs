798e14c75cd928485c7546cf618db29dcd27b7d6
Andreas Fackler <andreas.fackler@maidsafe.net>
2016-11-29 17:12:49 +0100

    feat/build: simplify CI scripts

--- src/states/common/bootstrapped.rs
+++ src/states/common/bootstrapped.rs
@@ -109,8 +109,8 @@ pub trait Bootstrapped: Base {
                        self,
                        unacked_msg);
                 self.stats().count_unacked();
-            } else if let Err(error) = self.send_routing_message_via_route(unacked_msg.routing_msg,
-                                                unacked_msg.route) {
+            } else if let Err(error) =
+                self.send_routing_message_via_route(unacked_msg.routing_msg, unacked_msg.route) {
                 debug!("{:?} Failed to send message: {:?}", self, error);
             }
         }
