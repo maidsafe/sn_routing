3c8fb95ba2a60056d281869e850cea549976872c
Bartłomiej Kamiński <fizyk20@gmail.com>
2017-01-03 14:42:28 +0100

    feat/node: accumulate signatures for our own section too (#1301)

--- src/states/node.rs
+++ src/states/node.rs
@@ -562,14 +562,28 @@ impl Node {
     fn send_section_list_signature(&mut self,
                                    prefix: Prefix<XorName>,
                                    dst: Option<XorName>)
-                                   -> Evented<Result<(), RoutingError>> {
+                                   -> Evented<()> {
         let mut result = Evented::empty();
-        // don't send signatures for our own group
-        if prefix == *self.peer_mgr.routing_table().our_prefix() {
-            return result.with_value(Ok(()));
-        }
-        let section = try_ev!(self.get_section_list(&prefix), result);
-        let serialised = try_ev!(serialisation::serialise(&section), result);
+        let section = match self.get_section_list(&prefix) {
+            Ok(section) => section,
+            Err(err) => {
+                warn!("{:?} Error sending section list signature for {:?}: {:?}",
+                      self,
+                      prefix,
+                      err);
+                return result;
+            }
+        };
+        let serialised = match serialisation::serialise(&section) {
+            Ok(serialised) => serialised,
+            Err(err) => {
+                warn!("{:?} Error sending section list signature for {:?}: {:?}",
+                      self,
+                      prefix,
+                      err);
+                return result;
+            }
+        };
         let sig = sign::sign_detached(&serialised, self.full_id.signing_private_key());
 
         self.section_list_sigs.add_signature(prefix,
@@ -604,7 +618,7 @@ impl Node {
             }
         }
 
-        result.map(Ok)
+        result
     }
 
     fn handle_section_list_signature(&mut self,
@@ -993,12 +1007,12 @@ impl Node {
         self.add_to_routing_table(public_id, peer_id).extract(&mut result);
 
         if let Some(prefix) = self.peer_mgr.routing_table().find_group_prefix(public_id.name()) {
-            let _ = self.send_section_list_signature(prefix, None).extract(&mut result);
+            self.send_section_list_signature(prefix, None).extract(&mut result);
             if prefix == *self.peer_mgr.routing_table().our_prefix() {
-                // if the node joined our group, send signatures for all neighbouring group lists
+                // if the node joined our section, send signatures for all section lists
                 // to it
-                for pfx in self.peer_mgr.routing_table().other_prefixes() {
-                    let _ = self.send_section_list_signature(pfx, Some(*public_id.name()))
+                for pfx in self.peer_mgr.routing_table().prefixes() {
+                    self.send_section_list_signature(pfx, Some(*public_id.name()))
                         .extract(&mut result);
                 }
             }
@@ -1629,12 +1643,8 @@ impl Node {
 
         let prefix0 = prefix.pushed(false);
         let prefix1 = prefix.pushed(true);
-        if let Err(e) = self.send_section_list_signature(prefix0, None).extract(&mut events) {
-            warn!("{:?} Error sending section list signature for {:?}: {:?}", self, prefix0, e);
-        }
-        if let Err(e) = self.send_section_list_signature(prefix1, None).extract(&mut events) {
-            warn!("{:?} Error sending section list signature for {:?}: {:?}", self, prefix1, e);
-        }
+        self.send_section_list_signature(prefix0, None).extract(&mut events);
+        self.send_section_list_signature(prefix1, None).extract(&mut events);
 
         events.with_value(Ok(()))
     }
@@ -1658,16 +1668,10 @@ impl Node {
                        self,
                        self.peer_mgr.routing_table().prefixes());
                 self.merge_if_necessary().extract(&mut result);
-                // after the merge, half of our section won't have our signatures for the
-                // neighbouring sections - send them
-                for prefix in self.peer_mgr.routing_table().other_prefixes() {
-                    if let Err(e) = self.send_section_list_signature(prefix, None)
-                        .extract(&mut result) {
-                        warn!("{:?} Error sending section list signature for prefix {:?}: {:?}",
-                              self,
-                              prefix,
-                              e);
-                    }
+                // after the merge, half of our section won't have our signatures -
+                // - send them
+                for prefix in self.peer_mgr.routing_table().prefixes() {
+                    self.send_section_list_signature(prefix, None).extract(&mut result);
                 }
                 let src = Authority::Section(self.peer_mgr
                     .routing_table()
@@ -1720,7 +1724,7 @@ impl Node {
                self,
                self.peer_mgr.routing_table().prefixes());
         self.merge_if_necessary().extract(&mut result);
-        try_evx!(self.send_section_list_signature(prefix, None), result);
+        self.send_section_list_signature(prefix, None).extract(&mut result);
         result.with_value(Ok(()))
     }
 
@@ -2113,20 +2117,10 @@ impl Node {
 
         self.merge_if_necessary().extract(&mut result);
 
-        if !details.was_in_our_group {
-            self.peer_mgr.routing_table().find_group_prefix(&details.name).map_or((), |prefix| {
-                let _ = self.send_section_list_signature(prefix, None).extract(&mut result);
-            });
-        } else {
-            self.section_list_sigs
-                .remove_signatures_by(*pub_id, self.peer_mgr.routing_table().our_section().len());
-        }
-
-        if !details.was_in_our_group {
-            self.peer_mgr.routing_table().find_group_prefix(&details.name).map_or((), |prefix| {
-                let _ = self.send_section_list_signature(prefix, None);
-            });
-        } else {
+        self.peer_mgr.routing_table().find_group_prefix(&details.name).map_or((), |prefix| {
+            self.send_section_list_signature(prefix, None).extract(&mut result);
+        });
+        if details.was_in_our_group {
             self.section_list_sigs
                 .remove_signatures_by(*pub_id, self.peer_mgr.routing_table().our_section().len());
         }
