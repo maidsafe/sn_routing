9f81979f7d5c3764f1ffa201ea91fe10d3d05749
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2018-02-19 10:11:01 +0000

    test/vote_accumulator: add unit tests for VoteAccumulator
    Also added quorum function and test to data_chain module.

--- src/data_chain/mod.rs
+++ src/data_chain/mod.rs
@@ -20,9 +20,92 @@ mod chain;
 mod node_state;
 mod proof;
 mod vote;
+mod vote_accumulator;
 
-pub use self::block::{Block, NodesAndAge};
+pub use self::block::{Block, BlockState, NodesAndAge};
 pub use self::chain::Chain;
 pub use self::node_state::{NodeState, State};
 pub use self::proof::Proof;
 pub use self::vote::Vote;
+pub use self::vote_accumulator::VoteAccumulator;
+
+use public_info::PublicInfo;
+use std::collections::BTreeSet;
+
+// FIXME - remove
+#[allow(unused)]
+/// Calculates whether a quorum of nodes have voted.  In this case, "quorum" means >50% of the
+/// members in `valid_nodes_itr` are included in `voters_itr` and that their cumulative age is >50%
+/// of the cumulative age of all members of `valid_nodes_itr`.
+pub fn quorum<'a, 'b, I, J>(voters_itr: I, valid_nodes_itr: J) -> bool
+where
+    I: Iterator<Item = &'a PublicInfo>,
+    J: Iterator<Item = &'b PublicInfo>,
+{
+    let valid_nodes = valid_nodes_itr.collect::<BTreeSet<_>>();
+    let voters = voters_itr.collect::<BTreeSet<_>>();
+
+    let valid_total_age = valid_nodes.iter().map(|node| usize::from(node.age())).sum();
+    let mut running_total_age = 0;
+    let mut count = 0;
+    for voter in voters.iter().rev() {
+        if valid_nodes.contains(voter) {
+            running_total_age += usize::from(voter.age());
+            count += 1;
+            if running_total_age * 2 > valid_total_age && count * 2 > valid_nodes.len() {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use full_info::FullInfo;
+    use maidsafe_utilities::SeededRng;
+    use rust_sodium;
+    use std::iter;
+
+    #[test]
+    fn quorum_check() {
+        // Create a set of voters with ages 5, 6, 7, etc.
+        let mut rng = SeededRng::thread_rng();
+        unwrap!(rust_sodium::init_with_rng(&mut rng));
+        let count = 10;
+        let mut nodes: Vec<_> = (0..count)
+            .map(|_| *FullInfo::node_new(0).public_info())
+            .collect();
+        nodes.sort();
+        for (index, node) in nodes.iter_mut().enumerate() {
+            node.set_age(index as u8 + 5);
+        }
+
+        // Check a majority of nodes, but comprising the youngest, don't get quorum.
+        assert!(!quorum(nodes.iter().take(count / 2 + 1), nodes.iter()));
+
+        // Check that a non-majority of eldest nodes don't get quorum.
+        assert!(!quorum(nodes.iter().rev().take(count / 2), nodes.iter()));
+
+        // Check that only valid voters are considered.
+        let invalid = *FullInfo::node_new(100).public_info();
+        assert!(!quorum(
+            nodes.iter().rev().take(count / 2).chain(
+                iter::once(&invalid),
+            ),
+            nodes.iter(),
+        ));
+
+        // Check that duplicate valid voters are counted only once.
+        assert!(!quorum(
+            nodes.iter().rev().take(count / 2).chain(iter::once(
+                &nodes[count - 1],
+            )),
+            nodes.iter(),
+        ));
+
+        // Check a majority of nodes with more than half the total age get quorum.
+        assert!(quorum(nodes.iter().rev().take(count / 2 + 1), nodes.iter()));
+    }
+}
