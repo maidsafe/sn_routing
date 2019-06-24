98a972a611b6f10d3a54d2276f6b2a6f593fb028
Adam Cigánek <adam.ciganek@gmail.com>
2019-06-24 13:57:24 +0200

    Remove ban_malicious_client integration test
    We no longer detect invalid messages from clients so this test wasn't relevant anymore.

--- tests/mock_network/client_restrictions.rs
+++ tests/mock_network/client_restrictions.rs
@@ -6,52 +6,8 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-use super::{
-    create_connected_clients, create_connected_nodes, poll_all, poll_and_resend, TestClient,
-    TestNode, MIN_SECTION_SIZE,
-};
-use rand::Rng;
-use routing::{
-    mock::Network, Authority, Event, EventStream, FullId, MessageId, NetworkConfig, Request,
-};
-
-/// Connect a client to the network then send an invalid message.
-/// Expect the client will be disconnected and banned;
-#[test]
-// TODO (quic-p2p): This test requires bootstrap blacklist which isn't implemented in quic-p2p.
-#[ignore]
-fn ban_malicious_client() {
-    let network = Network::new(MIN_SECTION_SIZE, None);
-    let mut nodes = create_connected_nodes(&network, MIN_SECTION_SIZE);
-    let mut clients = create_connected_clients(&network, &mut nodes, 1);
-    let mut rng = network.new_rng();
-
-    // Send a request with priority 1 from the client; should cause it to get banned.
-    let _ = clients[0].inner.send_request(
-        Authority::NaeManager(rng.gen()),
-        Request::Refresh(vec![], MessageId::new()),
-        1,
-    );
-    let _ = poll_all(&mut nodes, &mut clients);
-    expect_next_event!(unwrap!(clients.last_mut()), Event::Terminated);
-    let banned_client_ips = nodes[0]
-        .inner
-        .node_state_unchecked()
-        .get_banned_client_ips();
-    assert_eq!(banned_client_ips.len(), 1);
-    let endpoint = clients[0].endpoint();
-    assert_eq!(unwrap!(banned_client_ips.into_iter().next()), endpoint.ip());
-
-    let _ = clients.remove(0);
-    let _ = poll_all(&mut nodes, &mut clients);
-
-    // Connect a new client with the same ip address shall get rejected.
-    let config = NetworkConfig::client().with_hard_coded_contact(nodes[0].endpoint());
-    let client = TestClient::new(&network, Some(config), Some(endpoint));
-    clients.push(client);
-    let _ = poll_all(&mut nodes, &mut clients);
-    expect_next_event!(unwrap!(clients.last_mut()), Event::Terminated);
-}
+use super::{create_connected_nodes, poll_and_resend, TestClient, TestNode, MIN_SECTION_SIZE};
+use routing::{mock::Network, Event, EventStream, FullId, NetworkConfig};
 
 /// Reconnect a client (disconnected as network not having enough nodes) with the same id.
 #[test]
