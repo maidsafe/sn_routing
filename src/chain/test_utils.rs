9c9b2d6b596cf9255bc91030955136c381649c85
Jon Haggblad <jon.haggblad@gmail.com>
2019-10-04 10:42:32 +0200

    refactor/test_utils: rewrite verify_single_chain
    Rewrite verify_single_chain in terms of public API functions on Node.
    
    The test for checking `PrefixChange::None` was removed and needs to be
    replaced with explicit memberlist checks at a higher level.

--- src/chain/test_utils.rs
+++ src/chain/test_utils.rs
@@ -6,117 +6,10 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-use super::{Chain, EldersInfo, PrefixChange};
+use super::{Chain, EldersInfo};
 use crate::{Prefix, XorName};
-use std::collections::{BTreeMap, BTreeSet};
+use std::collections::BTreeMap;
 use std::iter;
-
-fn verify_single_chain(chain: &Chain, min_section_size: usize) {
-    assert!(
-        chain.our_info().prefix().matches(chain.our_id().name()),
-        "Our prefix doesn't match our name: {:?}, {:?}",
-        chain.our_info().prefix(),
-        chain.our_id().name()
-    );
-
-    assert_eq!(
-        chain.prefix_change(),
-        PrefixChange::None,
-        "{} has an unexpected prefix change: {:?}",
-        chain.our_id(),
-        chain.prefix_change()
-    );
-
-    if !chain.our_info().prefix().is_empty() {
-        assert!(
-            chain.our_info().members().len() >= min_section_size,
-            "Our section {:?} is below the minimum size!",
-            chain.our_info().prefix()
-        );
-    }
-
-    if let Some(name) = chain
-        .our_info()
-        .members()
-        .iter()
-        .find(|name| !chain.our_info().prefix().matches(name.name()))
-    {
-        panic!(
-            "A name in our section doesn't match its prefix! {:?}, {:?}",
-            name,
-            chain.our_info().prefix()
-        );
-    }
-
-    if let Some(info) = chain
-        .neighbour_infos()
-        .find(|info| info.prefix().is_compatible(chain.our_info().prefix()))
-    {
-        panic!(
-            "Our prefix is compatible with one of the neighbour prefixes:\
-             us: {:?} / neighbour: {:?}",
-            chain.our_info().prefix(),
-            info.prefix()
-        );
-    }
-
-    if let Some(info) = chain
-        .neighbour_infos()
-        .find(|info| info.members().len() < min_section_size)
-    {
-        panic!(
-            "A section is below the minimum size: size({:?}) = {}; For ({:?}: {:?})",
-            info.prefix(),
-            info.members().len(),
-            chain.our_id(),
-            chain.our_info().prefix(),
-        );
-    }
-
-    for info in chain.neighbour_infos() {
-        if let Some(name) = info
-            .members()
-            .iter()
-            .find(|name| !info.prefix().matches(name.name()))
-        {
-            panic!(
-                "A name in a section doesn't match its prefix! {:?}, {:?}",
-                name,
-                info.prefix()
-            );
-        }
-    }
-
-    let all_are_neighbours = chain
-        .neighbour_infos()
-        .all(|x| chain.our_info().prefix().is_neighbour(x.prefix()));
-    let all_neighbours_covered = {
-        let prefixes: BTreeSet<_> = chain.neighbour_infos().map(|info| *info.prefix()).collect();
-        (0..chain.our_info().prefix().bit_count()).all(|i| {
-            chain
-                .our_info()
-                .prefix()
-                .with_flipped_bit(i)
-                .is_covered_by(&prefixes)
-        })
-    };
-    if !all_are_neighbours {
-        panic!(
-            "Some sections in the chain aren't neighbours of our section: {:?}",
-            iter::once(chain.our_info().prefix())
-                .chain(chain.neighbour_infos().map(EldersInfo::prefix))
-                .collect::<Vec<_>>()
-        );
-    }
-    if !all_neighbours_covered {
-        panic!(
-            "Some neighbours aren't fully covered by the chain: {:?}",
-            iter::once(chain.our_info().prefix())
-                .chain(chain.neighbour_infos().map(EldersInfo::prefix))
-                .collect::<Vec<_>>()
-        );
-    }
-}
 
 /// Verifies that the given chains satisfy the network invariant.
 #[allow(unused)]
@@ -127,7 +20,6 @@ where
     let mut sections: BTreeMap<Prefix<XorName>, (XorName, EldersInfo)> = BTreeMap::new();
 
     for chain in chains {
-        verify_single_chain(chain, min_section_size);
         for prefix in iter::once(chain.our_info().prefix())
             .chain(chain.neighbour_infos().map(EldersInfo::prefix))
         {
