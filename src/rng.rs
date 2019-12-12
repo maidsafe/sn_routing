9320e5e35de709be3c59cd90fee9264530ee0588
qima <qi.ma@maidsafe.net>
2019-12-12 23:45:41 +0800

    feat/TestRng: impl RngCore trait for TestRng

--- src/rng.rs
+++ src/rng.rs
@@ -137,6 +137,25 @@ mod test {
         }
     }
 
+    impl rand_crypto::RngCore for TestRng {
+        fn next_u32(&mut self) -> u32 {
+            self.0.next_u32()
+        }
+
+        fn next_u64(&mut self) -> u64 {
+            self.0.next_u64()
+        }
+
+        fn fill_bytes(&mut self, dest: &mut [u8]) {
+            self.0.fill_bytes(dest)
+        }
+
+        fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), rand_crypto::Error> {
+            self.0.fill_bytes(dest);
+            Ok(())
+        }
+    }
+
     /// Seed for random number generators.
     #[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
     pub struct Seed([u32; 4]);
