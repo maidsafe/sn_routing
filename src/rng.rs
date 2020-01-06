eb661fdad04d77175a768f713e04c15279effc8c
Jean-Philippe DUFRAIGNE <jean.dufraigne@maidsafe.net>
2020-01-06 17:07:31 +0000

    feat/routing: add compatibility adapter from rand 0.7.2 to 0.4.2
    This is to be used when upgrading rand so we can give a generator to
    parsec.
    
    Test:
    clippy

--- src/rng.rs
+++ src/rng.rs
@@ -14,11 +14,9 @@ pub use self::seed_printer::SeedPrinter;
 #[cfg(any(test, feature = "mock_base"))]
 pub use self::test::Seed;
 
-use rand::Rng;
-
-// `CryptoRng` trait shim.
+// `Cryptorand::Rng` trait shim.
 // TODO: remove this when we update rand to more recent version as it has its own `CryptoRng` trait.
-pub(crate) trait CryptoRng: Rng {}
+pub(crate) trait CryptoRng: rand::Rng {}
 impl<'a, R: CryptoRng> CryptoRng for &'a mut R {}
 impl CryptoRng for MainRng {}
 
@@ -26,7 +24,7 @@ impl CryptoRng for MainRng {}
 // compatibility adapter between the two.
 pub(crate) struct RngCompat<R>(pub R);
 
-impl<R: Rng> rand_crypto::RngCore for RngCompat<R> {
+impl<R: rand::Rng> rand_crypto::RngCore for RngCompat<R> {
     fn next_u32(&mut self) -> u32 {
         self.0.next_u32()
     }
@@ -46,6 +44,16 @@ impl<R: Rng> rand_crypto::RngCore for RngCompat<R> {
 }
 
 impl<R: CryptoRng> rand_crypto::CryptoRng for RngCompat<R> {}
+
+// Note: routing uses different version of the rand crate than threshold_crypto. This is a
+// compatibility adapter between the two.
+pub(crate) struct RngParsecCompat<R>(pub R);
+
+impl<R: rand_new::Rng> rand_parsec::Rng for RngParsecCompat<R> {
+    fn next_u32(&mut self) -> u32 {
+        self.0.next_u32()
+    }
+}
 
 // Rng implementation used in production. Uses `OsRng` for maximum cryptographic security.
 #[cfg(not(any(test, feature = "mock_base")))]
