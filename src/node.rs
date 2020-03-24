fb374ac78474a7dc55cddc49854d62cdf647f388
Adam Cigánek <adam.ciganek@gmail.com>
2020-03-24 12:47:24 +0100

    Move src/states/approved_peer/mod.rs to src/node.rs

--- src/node.rs
+++ src/node.rs
@@ -6,8 +6,8 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-#[cfg(all(test, feature = "mock"))]
-mod tests;
+// #[cfg(all(test, feature = "mock"))]
+// mod tests;
 
 use crate::{
     action::Action,
