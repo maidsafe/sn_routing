7dae7c75bb99dc52dcfcdcf579bc78ad7ad43394
Adam Cigánek <adam.ciganek@gmail.com>
2020-03-24 12:47:24 +0100

    Move src/states/approved_peer/mod.rs to src/node.rs

--- src/states/mod.rs
+++ src/states/mod.rs
@@ -6,7 +6,4 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-mod approved_peer;
 mod joining_peer;
-
-pub use self::approved_peer::{Node, NodeConfig};
