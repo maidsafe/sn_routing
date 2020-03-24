7b8e829f22fecbdd431673b62c3483dd2b7b96db
Adam Cigánek <adam.ciganek@gmail.com>
2020-03-24 10:26:26 +0100

    Simplify Node and StateMachine creation

--- tests/mock_network/utils.rs
+++ tests/mock_network/utils.rs
@@ -195,7 +195,7 @@ impl<'a> TestNodeBuilder<'a> {
     pub fn create(self) -> TestNode {
         let (inner, user_event_rx, _client_rx) = self
             .inner
-            .network_cfg(self.env.network_cfg())
+            .network_params(self.env.network_cfg())
             .rng(&mut self.env.new_rng())
             .create();
 
