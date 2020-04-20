55ca9029e7e12e40922cf21da56b9f37ff5f0e4b
Adam Cigánek <adam.ciganek@gmail.com>
2020-04-20 11:43:26 +0200

    create the section module

---
+++ src/section/mod.rs
@@ -0,0 +1,11 @@
+// Copyright 2020 MaidSafe.net limited.
+//
+// This SAFE Network Software is licensed to you under The General Public License (GPL), version 3.
+// Unless required by applicable law or agreed to in writing, the SAFE Network Software distributed
+// under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied. Please review the Licences for the specific language governing
+// permissions and limitations relating to use of the SAFE Network Software.
+
+mod section_members;
+
+pub use self::section_members::SectionMembers;
