c924c91f5f8c9dc147323e97520c835afb5025e2
Adam Cigánek <adam.ciganek@gmail.com>
2020-04-21 11:49:37 +0200

    remove unused code

--- src/utils.rs
+++ src/utils.rs
@@ -6,38 +6,6 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-use std::{
-    fmt::{self, Display, Formatter},
-    time::Duration,
-};
-
-/// Display a "number" to the given number of decimal places
-pub trait DisplayDuration {
-    /// Construct a formattable object
-    fn display_secs(&self) -> DisplayDurObj;
-}
-
-impl DisplayDuration for Duration {
-    fn display_secs(&self) -> DisplayDurObj {
-        DisplayDurObj { dur: *self }
-    }
-}
-
-/// Display a number to the given number of decimal places
-pub struct DisplayDurObj {
-    dur: Duration,
-}
-
-impl Display for DisplayDurObj {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        let mut secs = self.dur.as_secs();
-        if self.dur.subsec_nanos() >= 500_000_000 {
-            secs += 1;
-        }
-        write!(f, "{} seconds", secs)
-    }
-}
-
 // Test utils
 
 /// If the iterator yields exactly one element, returns it. Otherwise panics.
@@ -55,26 +23,4 @@ where
         panic!("exactly one element expected, got more");
     }
     first
-}
-
-#[cfg(test)]
-mod tests {
-    use super::DisplayDuration;
-    use std::time::Duration;
-
-    #[test]
-    fn duration_formatting() {
-        assert_eq!(
-            format!("{}", Duration::new(653_105, 499_000_000).display_secs()),
-            "653105 seconds"
-        );
-        assert_eq!(
-            format!("{}", Duration::new(653_105, 500_000_000).display_secs()),
-            "653106 seconds"
-        );
-        assert_eq!(
-            format!("{}", Duration::new(0, 900_000_000).display_secs()),
-            "1 seconds"
-        );
-    }
 }
