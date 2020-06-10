acf8106bc0fcc01c87b50f138dd49b2a341215af
Adam Cigánek <adam.ciganek@gmail.com>
2020-06-10 10:55:59 +0200

    allow keeping compatible prefixes in the PrefixMap in some case
    This is in preparation to have the entries in the maps signed. The signature would sign both the key (the prefix) and the value which means we can no longer move an entry from its prefix to its child prefix when the sibling is inserted, because that would invalidate the signature. Because of this we now allow a prefix and its descendant(s) to exist simultaneously in the map as long as the descendants don't fully cover it. Once they do, the prefix is removed.

--- src/section/prefix_map.rs
+++ src/section/prefix_map.rs
@@ -13,56 +13,37 @@ use std::{
     iter::FromIterator,
 };
 
-/// Map whose keys are `Prefix`es with the additional invariant that no two prefixes that are
-/// compatible (one is extension of the other) can be in the map at the same time.
-#[derive(Default, Clone, Eq, PartialEq, Serialize, Deserialize)]
+/// Map whose keys are `Prefix`es.
+#[derive(Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub(crate) struct PrefixMap<T>(BTreeMap<Prefix<XorName>, T>);
 
 impl<T: Clone> PrefixMap<T> {
     /// Create empty `PrefixMap`.
     pub fn new() -> Self {
-        Self(Default::default())
+        Self::default()
     }
 
-    /// Inserts new entry into the map. Replaces previous entry at `prefix` or any of its ancestors
-    /// if it exists. Returns the previous prefix and value.
-    /// To maintain the invariant, if the map already contains a prefix that is descendant of
-    /// `prefix`, nothing is inserted and the passed `prefix` and `value` are returned.
-    pub fn insert(&mut self, prefix: Prefix<XorName>, value: T) -> Option<(Prefix<XorName>, T)> {
+    /// Inserts new entry into the map. Replaces previous entry at `prefix`.
+    /// Removes those ancestors of `prefix` that are now fully covered by their descendants.
+    pub fn insert(&mut self, prefix: Prefix<XorName>, value: T) -> Option<T> {
+        // Don't insert if any descendant is already present in the map.
         if self.descendants(&prefix).next().is_some() {
-            return Some((prefix, value));
+            return Some(value);
         }
 
-        let mut ancestor_entry: Option<(_, T)> = None;
-
-        for ancestor_prefix in prefix.ancestors() {
-            if let Some((_, ancestor_value)) = ancestor_entry.as_ref() {
-                let _ = self
-                    .0
-                    .insert(ancestor_prefix.sibling(), ancestor_value.clone());
-            } else {
-                ancestor_entry = self
-                    .0
-                    .remove(&ancestor_prefix)
-                    .map(|value| (ancestor_prefix, value));
-            }
-        }
-
-        let old_value = self.0.insert(prefix, value);
-
-        if let Some((_, ancestor_value)) = ancestor_entry.as_ref() {
-            let _ = self.0.insert(prefix.sibling(), ancestor_value.clone());
-        }
-
-        ancestor_entry.or_else(|| old_value.map(|old_value| (prefix, old_value)))
+        let old = self.0.insert(prefix, value);
+        self.prune(prefix.popped());
+        old
     }
 
     /// Get the value at `prefix`, if any.
+    #[cfg(test)]
     pub fn get(&self, prefix: &Prefix<XorName>) -> Option<&T> {
         self.0.get(prefix)
     }
 
-    /// Get the prefix and value at `prefix` or any of its ancestors.
+    /// Get the entry at `prefix` or any of its ancestors. In case of multiple matches, returns the
+    /// one with the longest prefix.
     pub fn get_equal_or_ancestor(
         &self,
         prefix: &Prefix<XorName>,
@@ -81,14 +62,23 @@ impl<T: Clone> PrefixMap<T> {
         }
     }
 
+    /// Get the entry at the prefix that matches `name`. In case of multiple matches, returns the
+    /// one with the longest prefix.
+    pub fn get_matching(&self, name: &XorName) -> Option<(&Prefix<XorName>, &T)> {
+        self.0
+            .iter()
+            .filter(|(prefix, _)| prefix.matches(name))
+            .max_by_key(|(prefix, _)| prefix.bit_count())
+    }
+
+    /// Returns whether the map contain at least one entry whose prefix matches `name`.
+    pub fn contains_matching(&self, name: &XorName) -> bool {
+        self.0.keys().any(|prefix| prefix.matches(name))
+    }
+
     /// Returns an iterator over the entries, in order by prefixes.
     pub fn iter(&self) -> btree_map::Iter<Prefix<XorName>, T> {
         self.0.iter()
-    }
-
-    /// Returns an iterator over the prefixes in the map, in sorted order.
-    pub fn keys(&self) -> btree_map::Keys<Prefix<XorName>, T> {
-        self.0.keys()
     }
 
     /// Returns an iterator over the values, in order by prefixes.
@@ -101,7 +91,7 @@ impl<T: Clone> PrefixMap<T> {
     pub fn descendants<'a>(
         &'a self,
         prefix: &'a Prefix<XorName>,
-    ) -> impl Iterator<Item = (&'a Prefix<XorName>, &'a T)> + 'a {
+    ) -> impl Iterator<Item = (&'a Prefix<XorName>, &'a T)> + Clone + 'a {
         // TODO: there might be a way to do this in O(logn) using BTreeMap::range
         self.0
             .iter()
@@ -123,6 +113,30 @@ impl<T: Clone> PrefixMap<T> {
         for key in to_remove {
             let _ = self.0.remove(&key);
         }
+    }
+
+    // Remove `prefix` and any of its ancestors if they are covered by their descendants.
+    // For example, if `(00)` and `(01)` are both in the map, we can remove `(0)` and `()`.
+    fn prune(&mut self, mut prefix: Prefix<XorName>) {
+        // TODO: can this be optimized?
+
+        loop {
+            if prefix.is_covered_by(self.descendants(&prefix).map(|(p, _)| p)) {
+                let _ = self.0.remove(&prefix);
+            }
+
+            if prefix.is_empty() {
+                break;
+            } else {
+                prefix = prefix.popped();
+            }
+        }
+    }
+}
+
+impl<T> Default for PrefixMap<T> {
+    fn default() -> Self {
+        Self(Default::default())
     }
 }
 
@@ -160,37 +174,62 @@ impl<T> From<PrefixMap<T>> for BTreeMap<Prefix<XorName>, T> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::rng;
+    use rand::Rng;
 
     #[test]
     fn insert_existing_prefix() {
         let mut map = PrefixMap::new();
         assert_eq!(map.insert(prefix("0"), 1), None);
-        assert_eq!(map.insert(prefix("0"), 2), Some((prefix("0"), 1)));
+        assert_eq!(map.insert(prefix("0"), 2), Some(1));
         assert_eq!(map.get(&prefix("0")), Some(&2));
     }
 
     #[test]
-    fn insert_direct_descendant_of_existing_prefix() {
+    fn insert_direct_descendants_of_existing_prefix() {
         let mut map = PrefixMap::new();
-        assert_eq!(map.insert(prefix("0"), 1), None);
-        assert_eq!(map.insert(prefix("00"), 2), Some((prefix("0"), 1)));
+        assert_eq!(map.insert(prefix("0"), 0), None);
 
+        // Insert the first sibling. Parent remain in the map.
+        assert_eq!(map.insert(prefix("00"), 1), None);
+        assert_eq!(map.get(&prefix("00")), Some(&1));
+        assert_eq!(map.get(&prefix("01")), None);
+        assert_eq!(map.get(&prefix("0")), Some(&0));
+
+        // Insert the other sibling. Parent is removed because it is now fully covered by its
+        // descendants.
+        assert_eq!(map.insert(prefix("01"), 2), None);
+        assert_eq!(map.get(&prefix("00")), Some(&1));
+        assert_eq!(map.get(&prefix("01")), Some(&2));
         assert_eq!(map.get(&prefix("0")), None);
-        assert_eq!(map.get(&prefix("00")), Some(&2));
-        assert_eq!(map.get(&prefix("01")), Some(&1));
     }
 
     #[test]
-    fn insert_indirect_descendant_of_existing_prefix() {
+    fn insert_indirect_descendants_of_existing_prefix() {
         let mut map = PrefixMap::new();
-        assert_eq!(map.insert(prefix("0"), 1), None);
-        assert_eq!(map.insert(prefix("000"), 2), Some((prefix("0"), 1)));
+        assert_eq!(map.insert(prefix("0"), 0), None);
 
-        assert_eq!(map.get(&prefix("0")), None);
+        assert_eq!(map.insert(prefix("000"), 1), None);
+        assert_eq!(map.get(&prefix("000")), Some(&1));
+        assert_eq!(map.get(&prefix("001")), None);
         assert_eq!(map.get(&prefix("00")), None);
-        assert_eq!(map.get(&prefix("000")), Some(&2));
-        assert_eq!(map.get(&prefix("001")), Some(&1));
-        assert_eq!(map.get(&prefix("01")), Some(&1));
+        assert_eq!(map.get(&prefix("01")), None);
+        assert_eq!(map.get(&prefix("0")), Some(&0));
+
+        assert_eq!(map.insert(prefix("001"), 2), None);
+        assert_eq!(map.get(&prefix("000")), Some(&1));
+        assert_eq!(map.get(&prefix("001")), Some(&2));
+        assert_eq!(map.get(&prefix("00")), None);
+        assert_eq!(map.get(&prefix("01")), None);
+        assert_eq!(map.get(&prefix("0")), Some(&0));
+
+        assert_eq!(map.insert(prefix("01"), 3), None);
+        assert_eq!(map.get(&prefix("000")), Some(&1));
+        assert_eq!(map.get(&prefix("001")), Some(&2));
+        assert_eq!(map.get(&prefix("00")), None);
+        assert_eq!(map.get(&prefix("01")), Some(&3));
+        // (0) is now fully covered and so was removed
+        assert_eq!(map.get(&prefix("0")), None);
     }
 
     #[test]
@@ -198,7 +237,7 @@ mod tests {
         let mut map = PrefixMap::new();
         let _ = map.insert(prefix("00"), 1);
 
-        assert_eq!(map.insert(prefix("0"), 2), Some((prefix("0"), 2)));
+        assert_eq!(map.insert(prefix("0"), 2), Some(2));
         assert_eq!(map.get(&prefix("0")), None);
         assert_eq!(map.get(&prefix("00")), Some(&1));
     }
@@ -230,6 +269,31 @@ mod tests {
         assert_eq!(
             map.get_equal_or_ancestor(&prefix("100")),
             Some((&prefix("10"), &1))
+        );
+    }
+
+    #[test]
+    fn get_matching() {
+        let mut rng = rng::new();
+
+        let mut map = PrefixMap::new();
+        let _ = map.insert(prefix("0"), 0);
+        let _ = map.insert(prefix("1"), 1);
+        let _ = map.insert(prefix("10"), 10);
+
+        assert_eq!(
+            map.get_matching(&prefix("0").substituted_in(rng.gen())),
+            Some((&prefix("0"), &0))
+        );
+
+        assert_eq!(
+            map.get_matching(&prefix("11").substituted_in(rng.gen())),
+            Some((&prefix("1"), &1))
+        );
+
+        assert_eq!(
+            map.get_matching(&prefix("10").substituted_in(rng.gen())),
+            Some((&prefix("10"), &10))
         );
     }
 
