4d568d1479d877939be17ce34b8cad6bc4cff61b
Adam Cigánek <adam.ciganek@gmail.com>
2020-07-08 15:42:30 +0200

    test: fix and un-ignore adult unit tests

--- src/node/tests/adult.rs
+++ src/node/tests/adult.rs
@@ -8,7 +8,7 @@
 
 use super::utils::{self as test_utils, MockTransport};
 use crate::{
-    consensus::{self, Proven},
+    consensus,
     error::Result,
     id::FullId,
     location::DstLocation,
@@ -16,14 +16,11 @@ use crate::{
     network_params::NetworkParams,
     node::{Node, NodeConfig},
     rng::{self, MainRng},
-    section::{
-        self, EldersInfo, SectionKeyShare, SectionKeysProvider, SectionProofChain, SharedState,
-    },
+    section::{self, EldersInfo, SectionProofChain, SharedState},
 };
 
 use mock_quic_p2p::Network;
 use rand::Rng;
-use std::net::SocketAddr;
 use xor_name::Prefix;
 
 const ELDER_SIZE: usize = 3;
@@ -36,8 +33,9 @@ struct Env {
     rng: MainRng,
     network: Network,
     subject: Node,
-    elders: Vec<Elder>,
     sk_set: bls::SecretKeySet,
+    elders_info: EldersInfo,
+    elder_full_ids: Vec<FullId>,
 }
 
 impl Env {
@@ -45,13 +43,12 @@ impl Env {
         let mut rng = rng::new();
         let network = Network::new();
 
-        let (elders_info, full_ids) =
+        let (elders_info, elder_full_ids) =
             section::gen_elders_info(&mut rng, Default::default(), ELDER_SIZE);
         let sk_set = consensus::generate_secret_key_set(&mut rng, ELDER_SIZE);
-        let elders_info = test_utils::create_proven(&sk_set, elders_info);
-        let elders = create_elders(&sk_set, elders_info.clone(), full_ids);
 
-        let shared_state = SharedState::new(elders_info);
+        let proven_elders_info = test_utils::create_proven(&sk_set, elders_info.clone());
+        let shared_state = SharedState::new(proven_elders_info);
 
         let (subject, ..) = Node::approved(
             NodeConfig {
@@ -67,8 +64,9 @@ impl Env {
             rng,
             network,
             subject,
-            elders,
             sk_set,
+            elders_info,
+            elder_full_ids,
         }
     }
 
@@ -76,53 +74,30 @@ impl Env {
         self.network.poll(&mut self.rng)
     }
 
-    fn perform_elders_change(&mut self) {
-        let elders_info = self.elders[0].state.our_info().clone();
-        let full_ids = self.elders.drain(..).map(|elder| elder.full_id).collect();
-
-        let new_sk_set = consensus::generate_secret_key_set(&mut self.rng, ELDER_SIZE);
-        let elders_info = test_utils::create_proven(&new_sk_set, elders_info);
-
-        self.elders = create_elders(&new_sk_set, elders_info, full_ids);
-    }
-
-    fn create_user_message(&self, dst: DstLocation, content: Vec<u8>) -> Message {
-        let msg = PlainMessage {
-            src: Prefix::default(),
-            dst,
-            dst_key: self.elders[0]
-                .section_keys_provider
-                .key_share()
-                .unwrap()
-                .public_key_set
-                .public_key(),
-            variant: Variant::UserMessage(content),
-        };
-        self.accumulate_message(msg)
-    }
-
-    fn accumulate_message(&self, content: PlainMessage) -> Message {
-        test_utils::accumulate_messages(
-            self.elders
-                .iter()
-                .map(|elder| to_accumulating_message(elder, content.clone()).unwrap()),
-        )
+    // Create `MockTransport` for the `index`-th elder.
+    fn create_elder_transport(&self, index: usize) -> MockTransport {
+        let addr = self
+            .elders_info
+            .elders
+            .get(self.elder_full_ids[index].public_id().name())
+            .map(|p2p_node| p2p_node.peer_addr());
+        MockTransport::new(addr)
     }
 
     fn send_elders_update(
         &mut self,
-        sender_full_id: &FullId,
-        elders_info: EldersInfo,
         parsec_version: u64,
         proof_chain: SectionProofChain,
     ) -> Result<()> {
-        let sender = *elders_info
+        let sender_full_id = &self.elder_full_ids[0];
+        let sender_addr = *self
+            .elders_info
             .elders
             .get(sender_full_id.public_id().name())
             .unwrap()
             .peer_addr();
 
-        let elders_info = test_utils::create_proven(&self.sk_set, elders_info);
+        let elders_info = test_utils::create_proven(&self.sk_set, self.elders_info.clone());
         let elders_update = EldersUpdate::new(elders_info, parsec_version, proof_chain);
         let variant = Variant::EldersUpdate(elders_update);
         let message = Message::single_src(
@@ -132,7 +107,27 @@ impl Env {
             variant,
         )?;
 
-        test_utils::handle_message(&mut self.subject, sender, message)
+        test_utils::handle_message(&mut self.subject, sender_addr, message)
+    }
+
+    fn create_user_message(&self, dst: DstLocation, content: Vec<u8>) -> Message {
+        let pk_set = self.sk_set.public_keys();
+        let pk = pk_set.public_key();
+
+        let plain_msg = PlainMessage {
+            src: Prefix::default(),
+            dst,
+            dst_key: pk,
+            variant: Variant::UserMessage(content),
+        };
+
+        let proof_chain = SectionProofChain::new(pk);
+
+        test_utils::accumulate_messages((0..ELDER_SIZE).map(|index| {
+            let sk_share = self.sk_set.secret_key_share(index);
+            let proof_share = plain_msg.prove(pk_set.clone(), index, &sk_share).unwrap();
+            AccumulatingMessage::new(plain_msg.clone(), proof_chain.clone(), proof_share)
+        }))
     }
 
     fn public_key(&self) -> bls::PublicKey {
@@ -140,88 +135,14 @@ impl Env {
     }
 }
 
-// Simplified representation of the section elder.
-struct Elder {
-    full_id: FullId,
-    state: SharedState,
-    section_keys_provider: SectionKeysProvider,
-    transport: MockTransport,
-}
-
-impl Elder {
-    fn addr(&self) -> &SocketAddr {
-        self.transport.addr()
-    }
-
-    fn received_messages(&self) -> impl Iterator<Item = (SocketAddr, Message)> + '_ {
-        self.transport.received_messages()
-    }
-}
-
-fn create_elders(
-    sk_set: &bls::SecretKeySet,
-    elders_info: Proven<EldersInfo>,
-    full_ids: Vec<FullId>,
-) -> Vec<Elder> {
-    let pk_set = sk_set.public_keys();
-
-    full_ids
-        .into_iter()
-        .enumerate()
-        .map(|(index, full_id)| {
-            let state = SharedState::new(elders_info.clone());
-
-            let section_keys_provider = SectionKeysProvider::new(Some(SectionKeyShare {
-                public_key_set: pk_set.clone(),
-                index,
-                secret_key_share: sk_set.secret_key_share(index),
-            }));
-
-            let addr = elders_info
-                .value
-                .elders
-                .get(full_id.public_id().name())
-                .map(|p2p_node| p2p_node.peer_addr());
-            let transport = MockTransport::new(addr);
-
-            Elder {
-                full_id,
-                state,
-                section_keys_provider,
-                transport,
-            }
-        })
-        .collect()
-}
-
-fn to_accumulating_message(sender: &Elder, content: PlainMessage) -> Result<AccumulatingMessage> {
-    let key_share = sender.section_keys_provider.key_share().unwrap();
-    let proof_chain = sender.state.prove(&content.dst, None);
-    let proof_share = content.prove(
-        key_share.public_key_set.clone(),
-        key_share.index,
-        &key_share.secret_key_share,
-    )?;
-
-    Ok(AccumulatingMessage::new(content, proof_chain, proof_share))
-}
-
 #[test]
 fn handle_elders_update_on_parsec_prune() {
     let mut env = Env::new();
 
-    let (elders_info, full_ids) =
-        section::gen_elders_info(&mut env.rng, Default::default(), ELDER_SIZE);
-
     assert_eq!(env.subject.parsec_last_version(), 0);
 
-    env.send_elders_update(
-        &full_ids[0],
-        elders_info,
-        1,
-        SectionProofChain::new(env.public_key()),
-    )
-    .unwrap();
+    env.send_elders_update(1, SectionProofChain::new(env.public_key()))
+        .unwrap();
     assert_eq!(env.subject.parsec_last_version(), 1);
 }
 
@@ -230,9 +151,6 @@ fn handle_elders_update_on_elders_change() {
     let mut env = Env::new();
 
     let old_pk = env.subject.section_key().copied();
-
-    let (elders_info, full_ids) =
-        section::gen_elders_info(&mut env.rng, Default::default(), ELDER_SIZE);
 
     let mut proof_chain = SectionProofChain::new(env.public_key());
 
@@ -243,8 +161,7 @@ fn handle_elders_update_on_elders_change() {
     proof_chain.push(new_pk, new_pk_proof.signature);
 
     env.sk_set = new_sk_set;
-    env.send_elders_update(&full_ids[0], elders_info, 1, proof_chain)
-        .unwrap();
+    env.send_elders_update(1, proof_chain).unwrap();
 
     assert_eq!(env.subject.parsec_last_version(), 1);
     assert_ne!(env.subject.section_key(), old_pk.as_ref());
@@ -255,21 +172,15 @@ fn handle_elders_update_on_elders_change() {
 fn handle_elders_update_ignore_old_parsec_vesions() {
     let mut env = Env::new();
 
-    let (elders_info, full_ids) =
-        section::gen_elders_info(&mut env.rng, Default::default(), ELDER_SIZE);
-
     let proof_chain = SectionProofChain::new(env.public_key());
 
-    env.send_elders_update(&full_ids[0], elders_info.clone(), 1, proof_chain.clone())
-        .unwrap();
+    env.send_elders_update(1, proof_chain.clone()).unwrap();
     assert_eq!(env.subject.parsec_last_version(), 1);
 
-    env.send_elders_update(&full_ids[0], elders_info.clone(), 2, proof_chain.clone())
-        .unwrap();
+    env.send_elders_update(2, proof_chain.clone()).unwrap();
     assert_eq!(env.subject.parsec_last_version(), 2);
 
-    env.send_elders_update(&full_ids[0], elders_info, 1, proof_chain)
-        .unwrap();
+    env.send_elders_update(1, proof_chain).unwrap();
     assert_eq!(env.subject.parsec_last_version(), 2);
 }
 
@@ -279,16 +190,8 @@ fn handle_elders_update_allow_skipped_parsec_versions() {
 
     assert_eq!(env.subject.parsec_last_version(), 0);
 
-    let (elders_info, full_ids) =
-        section::gen_elders_info(&mut env.rng, Default::default(), ELDER_SIZE);
-
-    env.send_elders_update(
-        &full_ids[0],
-        elders_info,
-        2,
-        SectionProofChain::new(env.public_key()),
-    )
-    .unwrap();
+    env.send_elders_update(2, SectionProofChain::new(env.public_key()))
+        .unwrap();
     assert_eq!(env.subject.parsec_last_version(), 2);
 }
 
@@ -299,14 +202,11 @@ fn untrusted_elders_update() {
     assert_eq!(env.subject.parsec_last_version(), 0);
     let old_pk = env.subject.section_key().copied();
 
-    let (elders_info, full_ids) =
-        section::gen_elders_info(&mut env.rng, Default::default(), ELDER_SIZE);
-
     let new_sk_set = consensus::generate_secret_key_set(&mut env.rng, ELDER_SIZE);
     let new_pk = new_sk_set.public_keys().public_key();
 
     env.sk_set = new_sk_set;
-    env.send_elders_update(&full_ids[0], elders_info, 1, SectionProofChain::new(new_pk))
+    env.send_elders_update(1, SectionProofChain::new(new_pk))
         .unwrap();
 
     assert_eq!(env.subject.parsec_last_version(), 0);
@@ -314,17 +214,19 @@ fn untrusted_elders_update() {
 }
 
 #[test]
-#[ignore] //FIXME No more StartDkg message, hence no BouncedUnknownMessage
 fn handle_unknown_message() {
     let mut env = Env::new();
 
+    // adults can't handle messages addressed to sections.
     let dst = DstLocation::Section(env.rng.gen());
     let msg = env.create_user_message(dst, b"hello section".to_vec());
-    test_utils::handle_message(&mut env.subject, *env.elders[0].addr(), msg).unwrap();
+    let transport = env.create_elder_transport(0);
+
+    test_utils::handle_message(&mut env.subject, *transport.addr(), msg).unwrap();
 
     env.poll();
 
-    for (sender, msg) in env.elders[0].received_messages() {
+    for (sender, msg) in transport.received_messages() {
         if sender == env.subject.our_connection_info().unwrap()
             && matches!(msg.variant(), Variant::BouncedUnknownMessage { .. })
         {
@@ -340,16 +242,18 @@ fn handle_untrusted_accumulated_message() {
     let mut env = Env::new();
 
     // Generate new section key which the adult is not yet aware of.
-    env.perform_elders_change();
+    env.sk_set = consensus::generate_secret_key_set(&mut env.rng, ELDER_SIZE);
 
     // This message is signed with the new key so won't be trusted by the adult yet.
     let dst = DstLocation::Node(*env.subject.name());
     let msg = env.create_user_message(dst, b"hello node".to_vec());
-    test_utils::handle_message(&mut env.subject, *env.elders[0].addr(), msg).unwrap();
+    let transport = env.create_elder_transport(0);
+
+    test_utils::handle_message(&mut env.subject, *transport.addr(), msg).unwrap();
 
     env.poll();
 
-    for (sender, msg) in env.elders[0].received_messages() {
+    for (sender, msg) in transport.received_messages() {
         if sender == env.subject.our_connection_info().unwrap()
             && matches!(msg.variant(), Variant::BouncedUntrustedMessage(_))
         {
@@ -359,29 +263,3 @@ fn handle_untrusted_accumulated_message() {
 
     panic!("BouncedUntrustedMessage not received")
 }
-
-/*
-#[test]
-#[ignore] //FIXME No more StartDkg message, hence no BouncedUntrustedMessage
-fn handle_untrusted_accumulating_message() {
-    let mut env = Env::new();
-    env.perform_elders_change();
-
-    // The GenesisUpdate message is accumulated at destination and so is received as a series of
-    // `MessageSignature`s.
-    let genesis_prefix_info = env.genesis_prefix_info(1);
-    env.handle_genesis_update(genesis_prefix_info).unwrap();
-
-    env.poll();
-
-    for (sender, msg) in env.elders[0].received_messages() {
-        if sender == env.subject.our_connection_info().unwrap()
-            && matches!(msg.variant(), Variant::BouncedUntrustedMessage(_))
-        {
-            return;
-        }
-    }
-
-    panic!("BouncedUntrustedMessage not received")
-}
-*/
