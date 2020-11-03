d1c3792287e0011513db417169a091c625e69e96
maqi <maqi@users.noreply.github.com>
2020-11-03 12:01:17 +0000

    deploy: 9dfb935afd9bdfe4dcc65d37e1cdbb93ac21fa06

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["sn_routing"] = [{"text":"impl Default for NetworkParams","synthetic":false,"types":[]},{"text":"impl Default for Config","synthetic":false,"types":[]}];
+implementors["sn_routing"] = [{"text":"impl Default for Config","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
