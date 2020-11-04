91215c1f5e6bac3fd94001af157581cf39960ba3
madadam <madadam@users.noreply.github.com>
2020-11-04 13:17:20 +0000

    deploy: 4dfd981943d6184bea80459c13400e75221a207d

---
+++ implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["sn_routing"] = [{"text":"impl Drop for Routing","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
