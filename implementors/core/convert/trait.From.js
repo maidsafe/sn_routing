9482d99094137d850ae6dbb76ff567f648b9db38
S-Coyle <S-Coyle@users.noreply.github.com>
2021-01-05 17:31:27 +0000

    deploy: 00f84fa4488565ea6559505cb628ef21de7633bf

--- implementors/core/convert/trait.From.js
+++ implementors/core/convert/trait.From.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["sn_routing"] = [{"text":"impl From&lt;Error&gt; for Error","synthetic":false,"types":[]},{"text":"impl From&lt;Box&lt;ErrorKind&gt;&gt; for Error","synthetic":false,"types":[]}];
+implementors["sn_routing"] = [{"text":"impl From&lt;Error&gt; for Error","synthetic":false,"types":[]},{"text":"impl From&lt;Box&lt;ErrorKind, Global&gt;&gt; for Error","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
