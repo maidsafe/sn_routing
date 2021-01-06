0b56abd88bfa827e4595e69d7a2667cd0b355d31
Stephen Coyle <stephen.coyle@maidsafe.net>
2021-01-06 15:23:50 +0000

    chore(docs): apply cargo intraconv changes to doc links
    cargo intraconv changes markdown links to intra-doc links

--- src/routing/mod.rs
+++ src/routing/mod.rs
@@ -70,7 +70,7 @@ impl Default for Config {
 /// A node is a part of the network that can route messages and be a member of a section or group
 /// location. Its methods can be used to send requests and responses as either an individual
 /// `Node` or as a part of a section or group location. Their `src` argument indicates that
-/// role, and can be any [`SrcLocation`](enum.SrcLocation.html).
+/// role, and can be any [`SrcLocation`].
 pub struct Routing {
     stage: Arc<Stage>,
 }
