7379a907936dadbb20053e45815a8453df77a2c4
oetyng <oetyng@users.noreply.github.com>
2021-02-23 16:32:50 +0000

    deploy: 31f7e648b6a0409a5aa646290dea24b278f635f2

--- implementors/core/hash/trait.Hash.js
+++ implementors/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["sn_routing"] = [{"text":"impl Hash for SrcLocation","synthetic":false,"types":[]},{"text":"impl Hash for DstLocation","synthetic":false,"types":[]},{"text":"impl Hash for SectionProofChain","synthetic":false,"types":[]}];
+implementors["sn_routing"] = [{"text":"impl Hash for SectionProofChain","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/serde/ser/trait.Serialize.js
+++ implementors/serde/ser/trait.Serialize.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["sn_routing"] = [{"text":"impl Serialize for SrcLocation","synthetic":false,"types":[]},{"text":"impl Serialize for DstLocation","synthetic":false,"types":[]},{"text":"impl Serialize for SectionProofChain","synthetic":false,"types":[]}];
+implementors["sn_routing"] = [{"text":"impl Serialize for SectionProofChain","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
