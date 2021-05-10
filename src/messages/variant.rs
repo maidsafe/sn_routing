4543a92b8a5178302174a5879954a432e2b77a37
oetyng <oetyng@gmail.com>
2021-05-10 12:53:09 +0200

    fix: post-rebase issues

--- src/messages/variant.rs
+++ src/messages/variant.rs
@@ -6,7 +6,7 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-use super::{Message, MessageHash, VerifyStatus};
+use super::{Message, VerifyStatus};
 use crate::{
     agreement::{DkgFailureProof, DkgFailureProofSet, DkgKey, ProofShare, Proposal, Proven},
     crypto::Signature,
