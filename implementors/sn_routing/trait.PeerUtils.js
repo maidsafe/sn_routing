2e05185e56570b75baf21bd7b3d2ef917ee4d408
lionel1704 <lionel1704@users.noreply.github.com>
2021-05-30 10:45:11 +0000

    deploy: 0934014e47eba92313dd9389351c8266a5b4fdf0

---
+++ implementors/sn_routing/trait.PeerUtils.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["sn_routing"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
