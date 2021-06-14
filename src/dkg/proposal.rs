dirvine/Routing

chore(rename) mod Agreement -> Dkg

src/dkg/proposal.rs
@@ -92,7 +92,7 @@ pub enum ProposalError {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{agreement, section};
+    use crate::{dkg, section};
     use anyhow::Result;
     use std::fmt::Debug;
     use xor_name::Prefix;
@@ -108,7 +108,7 @@ mod tests {
         // Proposal::OurElders
         let new_sk = bls::SecretKey::random();
         let new_pk = new_sk.public_key();
-        let proven_section_auth = agreement::test_utils::proven(&new_sk, section_auth)?;
+        let proven_section_auth = dkg::test_utils::proven(&new_sk, section_auth)?;
         let proposal = Proposal::OurElders(proven_section_auth);
         verify_serialize_for_signing(&proposal, &new_pk)?;
 

src/messages/mod.rs
@@ -11,7 +11,7 @@ mod src_authority;
 
 pub use self::{plain_message::PlainMessageUtils, src_authority::SrcAuthorityUtils};
 use crate::{
-    agreement::ProvenUtils,
+    dkg::ProvenUtils,
     ed25519::{self, Verifier},
     error::{Error, Result},
     node::Node,

src/network/mod.rs
@@ -12,7 +12,7 @@ mod stats;
 
 use self::stats::NetworkStats;
 use crate::{
-    agreement::{verify_signed, ProvenUtils, Signed},
+    dkg::{verify_signed, ProvenUtils, Signed},
     peer::PeerUtils,
     section::SectionAuthorityProviderUtils,
     Error, Result,
@@ -266,7 +266,7 @@ impl OtherSectionUtils for OtherSection {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{agreement, section};
+    use crate::{dkg, section};
     use rand::Rng;
 
     #[test]
@@ -298,6 +298,6 @@ mod tests {
         prefix: Prefix,
     ) -> Proven<SectionAuthorityProvider> {
         let (section_auth, _, _) = section::test_utils::gen_section_authority_provider(prefix, 5);
-        agreement::test_utils::proven(sk, section_auth).unwrap()
+        dkg::test_utils::proven(sk, section_auth).unwrap()
     }
 }

src/relocation.rs
@@ -268,7 +268,7 @@ fn trailing_zeros(bytes: &[u8]) -> u32 {
 mod tests {
     use super::*;
     use crate::{
-        agreement::test_utils::proven,
+        dkg::test_utils::proven,
         peer::test_utils::arbitrary_unique_peers,
         routing::tests::SecretKeySet,
         section::{MemberInfoUtils, SectionAuthorityProviderUtils},

src/routing/core/anti_entropy.rs
@@ -75,7 +75,7 @@ pub(crate) struct Actions {
 mod tests {
     use super::*;
     use crate::{
-        agreement::test_utils::proven,
+        dkg::test_utils::proven,
         ed25519,
         section::test_utils::{gen_addr, gen_section_authority_provider},
         XorName, ELDER_SIZE, MIN_ADULT_AGE,

src/routing/core/delivery_group.rs
@@ -166,7 +166,7 @@ fn get_peer(name: &XorName, section: &Section, network: &Network) -> Option<Peer
 mod tests {
     use super::*;
     use crate::{
-        agreement::test_utils::proven,
+        dkg::test_utils::proven,
         ed25519,
         section::{
             test_utils::{gen_addr, gen_section_authority_provider},

src/routing/core/messaging/handling/agreement.rs
@@ -9,7 +9,7 @@
 use std::cmp;
 
 use crate::{
-    agreement::ProvenUtils,
+    dkg::ProvenUtils,
     error::Result,
     messages::RoutingMsgUtils,
     network::NetworkUtils,

maidsafe/sn_routing

src/dkg/proposal.rs
@@ -92,7 +92,7 @@ pub enum ProposalError {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{agreement, section};
+    use crate::{dkg, section};
     use anyhow::Result;
     use std::fmt::Debug;
     use xor_name::Prefix;
@@ -108,9 +108,8 @@ mod tests {
         // Proposal::OurElders
         let new_sk = bls::SecretKey::random();
         let new_pk = new_sk.public_key();
-        let section_signed_section_auth =
-            agreement::test_utils::section_signed(&new_sk, section_auth)?;
-        let proposal = Proposal::OurElders(section_signed_section_auth);
+        let section_signed_auth = dkg::test_utils::section_signed(&new_sk, section_auth)?;
+        let proposal = Proposal::OurElders(section_signed_auth);
         verify_serialize_for_signing(&proposal, &new_pk)?;
 
         Ok(())

src/messages/mod.rs
@@ -11,7 +11,7 @@ mod src_authority;
 
 pub use self::{plain_message::PlainMessageUtils, src_authority::SrcAuthorityUtils};
 use crate::{
-    agreement::SectionSignedUtils,
+    dkg::SectionSignedUtils,
     ed25519::{self, Verifier},
     error::{Error, Result},
     node::Node,

src/network/mod.rs
@@ -12,7 +12,7 @@ mod stats;
 
 use self::stats::NetworkStats;
 use crate::{
-    agreement::{verify_signed, SectionSignedUtils, Signed},
+    dkg::{verify_signed, SectionSignedUtils, Signed},
     peer::PeerUtils,
     section::SectionAuthorityProviderUtils,
     Error, Result,
@@ -266,7 +266,7 @@ impl OtherSectionUtils for OtherSection {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{agreement, section};
+    use crate::{dkg, section};
     use rand::Rng;
 
     #[test]
@@ -300,6 +300,6 @@ mod tests {
         prefix: Prefix,
     ) -> Result<SectionSigned<SectionAuthorityProvider>> {
         let (section_auth, _, _) = section::test_utils::gen_section_authority_provider(prefix, 5);
-        agreement::test_utils::section_signed(sk, section_auth)
+        dkg::test_utils::section_signed(sk, section_auth)
     }
 }

src/relocation.rs
@@ -268,7 +268,7 @@ fn trailing_zeros(bytes: &[u8]) -> u32 {
 mod tests {
     use super::*;
     use crate::{
-        agreement::test_utils::section_signed,
+        dkg::test_utils::section_signed,
         peer::test_utils::arbitrary_unique_peers,
         routing::tests::SecretKeySet,
         section::{NodeStateUtils, SectionAuthorityProviderUtils},

src/routing/core/anti_entropy.rs
@@ -75,7 +75,7 @@ pub(crate) struct Actions {
 mod tests {
     use super::*;
     use crate::{
-        agreement::test_utils::section_signed,
+        dkg::test_utils::section_signed,
         ed25519,
         section::test_utils::{gen_addr, gen_section_authority_provider},
         XorName, ELDER_SIZE, MIN_ADULT_AGE,

src/routing/core/delivery_group.rs
@@ -166,7 +166,7 @@ fn get_peer(name: &XorName, section: &Section, network: &Network) -> Option<Peer
 mod tests {
     use super::*;
     use crate::{
-        agreement::test_utils::section_signed,
+        dkg::test_utils::section_signed,
         ed25519,
         section::{
             test_utils::{gen_addr, gen_section_authority_provider},

src/routing/core/messaging/handling/agreement.rs
@@ -9,7 +9,7 @@
 use std::cmp;
 
 use crate::{
-    agreement::SectionSignedUtils,
+    dkg::SectionSignedUtils,
     error::Result,
     messages::RoutingMsgUtils,
     network::NetworkUtils,